3541376fb3e196d8c33f3d6298b3951b24313336
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-12 19:47:50 +0530

    create AbstractClothList

---
+++ src/jan12_21/abstractclass/AbstractClothList.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 12-Jan-21
+ *   Time: 7:47 PM
+ *   File: AbstractClothList.java
+ */
+
+package jan12_21.abstractclass;
+
+public class AbstractClothList {
+}
+
