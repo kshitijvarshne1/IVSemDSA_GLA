a836eac9976f343b3ebb93fa5eda0ad0aecf7b60
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-12 19:48:22 +0530

    create ClothList

---
+++ src/jan12_21/concreteclass/ClothList.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 12-Jan-21
+ *   Time: 7:48 PM
+ *   File: ClothList.java
+ */
+
+package jan12_21.concreteclass;
+
+public class ClothList {
+}
+
