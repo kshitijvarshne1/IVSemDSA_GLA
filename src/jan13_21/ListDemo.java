29c87202ba9391db5b35467a1e70a41227126325
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-13 13:34:37 +0530

    implement arraylist and linkedlist methods

--- src/jan13_21/ListDemo.java
+++ src/jan13_21/ListDemo.java
@@ -7,6 +7,37 @@
 
 package jan13_21;
 
-public class ListDemo {
-}
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+public class ListDemo {
+    public static void main(String[] args) {
+        //ArrayList
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        //Traverse type
+        //1.Using a loop(indexed for loop)
+        for (int i = 0; i < list1.size(); i++) {
+            System.out.print(list1.get(i) + " ");
+        }
+        System.out.println();
+        //2. using enhanced for loop
+        for (Integer integer : list1) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+        //3.using collection framework
+        System.out.println(list1);
+        //4.using lambda programming
+        list1.stream().forEach(element -> System.out.println(element + " "));
+
+        //linkedlist
+        LinkedList<Integer> list2 = new LinkedList<>();
+        list2.add(4);
+        list2.add(3);
+        for (int i = 0; i < list2.size(); i++) {
+            System.out.print(list2.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
