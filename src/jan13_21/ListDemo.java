95736a86080adc14f9cdb33533877aba5f34a97c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-13 13:19:05 +0530

    create ListDemo.java

---
+++ src/jan13_21/ListDemo.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-Jan-21
+ *   Time: 1:18 PM
+ *   File: ListDemo.java
+ */
+
+package jan13_21;
+
+public class ListDemo {
+}
+
