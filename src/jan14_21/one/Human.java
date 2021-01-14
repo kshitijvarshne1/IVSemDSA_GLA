e48728142d5e9107259aa0e51cc059caa713f673
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-14 10:53:38 +0530

    create Student.java

--- src/jan14_21/one/Human.java
+++ src/jan14_21/one/Human.java
@@ -5,12 +5,13 @@ public interface Human {
     // By default all attribute of any interface is static, public and final
 
     void breathe();
+    //By default method is public and abstract
 }
 
 //Man is a Human
 abstract class Man implements Human {
     @Override
     public void breathe() {
-
+        System.out.println("Man breathe");
     }
 }
---
+++ src/jan14_21/one/Student.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 14-Jan-21
+ *   Time: 10:53 AM
+ *   File: Student.java
+ */
+
+package jan14_21.one;
+
+public class Student {
+}
+
