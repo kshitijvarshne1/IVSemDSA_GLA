c3652bdc19ccc3fcea1f927f5b16fcc807589534
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-18 10:42:13 +0530

    create program for find average of the marks

--- src/jan18_21_NK/one/StudentMain.java
+++ src/jan18_21_NK/one/StudentMain.java
@@ -7,9 +7,29 @@
 
 package jan18_21_NK.one;
 
+import java.util.Scanner;
+
 public class StudentMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int limit = sc.nextInt();
+        Student[] studentArray = new Student[5];
+        for (int i = 0; i < limit; i++) {
+            int id = sc.nextInt();
+            int marks = sc.nextInt();
+            sc.nextLine();
+            String studentName = sc.nextLine();
+            Student obj = new Student(id, marks, studentName);
+            studentArray[i] = obj;
+        }
+        sc.close();
+        int sum = 0;
+        for (int i = 0; i < limit; i++) {
+            sum += studentArray[i].getMarks();
 
+        }
+        float average = sum / limit;
+        System.out.println("Average :- " + average);
     }
 }
 
