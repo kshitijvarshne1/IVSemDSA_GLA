494c89619375f07329047f592ac0307f1b094255
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-18 11:28:52 +0530

    create StudentMain.java

---
+++ src/jan18_21_NK/two/StudentMain.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 18-Jan-21
+ *   Time: 11:28 AM
+ *   File: StudentMain.java
+ */
+
+package jan18_21_NK.two;
+
+public class StudentMain {
+}
+
