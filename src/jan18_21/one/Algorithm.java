6f93f120c5b2bb6e027cc9c73c4145f1e107aaab
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-18 12:57:33 +0530

    create Algorithm.java

---
+++ src/jan18_21/one/Algorithm.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 18-Jan-21
+ *   Time: 12:57 PM
+ *   File: Algorithm.java
+ */
+
+package jan18_21.one;
+
+public class Algorithm {
+}
+
