9e8e40c2fe9566ffb61f8a51878d54c1577ed748
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-18 13:38:04 +0530

    final commit

--- src/jan18_21/one/Algorithm.java
+++ src/jan18_21/one/Algorithm.java
@@ -10,3 +10,28 @@ package jan18_21.one;
 public class Algorithm {
 }
 
+/*
+ * Algorithm:---
+ *   Basic requirement:--
+ *       1.should have finite steps(should be properly defined)
+ *       2.should always produce output
+ *   Measure of Algorithm:--
+ *       1.Time complexity
+ *           1.Number of steps in execution
+ *           f(n)-> function on the input size.     n = input size
+ *           5 Notation:-
+ *               1.O (Big-O) =>> upper bound
+ *               2.Ω (Big - omega)
+ *               3.θ (Thetha)
+ *               4.o (small -o)
+ *               5.w (small - omega)
+ *           1.Constant Time:-  O(1) -> o of 1
+ *           2.Linear Time:- O(n),O(n/2)
+ *           3.Quadratc  :- O(n^2),(m*n)
+ *
+ *
+ *
+ *       2.Space(Memory) complexity
+ *
+ *
+ * */
