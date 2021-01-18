b54d365b6d27278285b3536441942133e680bbdb
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-18 11:28:24 +0530

    create private fields for Student.java and make constructor , getter and setter method for Student.jav a

--- src/jan18_21_NK/two/Student.java
+++ src/jan18_21_NK/two/Student.java
@@ -8,5 +8,54 @@
 package jan18_21_NK.two;
 
 public class Student {
+    private static String universityName;
+    private static String nameOfStream;
+
+    static {
+        universityName = "GLA";
+        nameOfStream = "B.Tech";
+    }
+
+    private int id;
+    private int marks;
+    private String studentName;
+
+    public Student(int id, int marks, String studentName) {
+        this.id = id;
+        this.marks = marks;
+        this.studentName = studentName;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public String getNameOfStream() {
+        return nameOfStream;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
 }
 
