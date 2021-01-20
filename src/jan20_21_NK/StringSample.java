46434ab55ac0b6ce57406f92284e24ea18659cc4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-20 21:50:50 +0530

    add implements of logic to check array of one string is equal to another array of string

--- src/jan20_21_NK/StringSample.java
+++ src/jan20_21_NK/StringSample.java
@@ -7,9 +7,27 @@
 
 package jan20_21_NK;
 
+import java.util.Scanner;
+
 public class StringSample {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s1;
+        String[] s2;
+        s1 = new String[10];
+        s2 = new String[10];
+        for (int i = 0; i < 10; i++) {
+            s1[i] = sc.nextLine();
+            s2[i] = sc.nextLine();
+        }
+        for (int i = 0; i < 10; i++) {
+            if (s1[i].equals(s2[i])) {
+                System.out.println(s1[i]);
+            } else {
+                System.out.println(s1[i] + " " + s2[i]);
+            }
 
+        }
     }
 }
 
