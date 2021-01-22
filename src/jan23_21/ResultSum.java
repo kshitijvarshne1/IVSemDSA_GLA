ef04c5cd868cade04fdf9d1f2ed636d420cc236b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-23 00:01:19 +0530

    create ResultSum.java

---
+++ src/jan23_21/ResultSum.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 23-Jan-21
+ *   Time: 12:01 AM
+ *   File: ResultSum.java
+ */
+
+package jan23_21;
+
+public class ResultSum {
+}
+
