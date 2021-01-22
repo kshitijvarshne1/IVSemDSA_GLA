61831472088437ad9034d528abd4b020e16298d0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-23 00:30:57 +0530

    add implementation of ResultSum

--- src/jan23_21/ResultSum.java
+++ src/jan23_21/ResultSum.java
@@ -7,8 +7,40 @@
 
 package jan23_21;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class ResultSum {
     public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int temp = (int) (Math.random() * 10);
+            arr.add(temp);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+        Collections.sort(arr);
+        final int RESULTSUM = 10;
+        int start = 0;
+        HashMap<Integer, Integer> result = new HashMap<>();
+        int last = arr.size() - 1;
+        while (start < last) {
+            if (arr.get(start) + arr.get(last) == RESULTSUM) {
+                result.put(arr.get(start), arr.get(last));
+                start++;
+                last--;
+            } else if (arr.get(start) + arr.get(last) > RESULTSUM) {
+                last--;
+            } else {
+                start++;
+            }
+        }
+        for (Integer k : result.keySet()) {
+            System.out.println(k + " " + result.get(k));
+        }
 
     }
 }
