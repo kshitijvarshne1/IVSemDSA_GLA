e62aac44475938b70f595654cca0ff5e01aacf25
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-26 19:39:35 +0530

    create FizzBuzzSimple.java

---
+++ src/jan19_21_NK/FizzBuzzSimple.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 26-Jan-21
+ *   Time: 7:39 PM
+ *   File: FizzBuzzSimple.java
+ */
+
+package jan19_21_NK;
+
+public class FizzBuzzSimple {
+}
+
