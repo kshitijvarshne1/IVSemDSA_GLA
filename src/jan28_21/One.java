a2c5f83b6bebdc948bc97fed1c7b9d328fca335a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-01-28 12:19:42 +0530

    add sample code to One.java for question Unique Characters

--- src/jan28_21/One.java
+++ src/jan28_21/One.java
@@ -7,6 +7,22 @@
 
 package jan28_21;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class One {
+    public static final int MAX_UNICODE = 65_535;
+
+    public static void main(String[] args) {
+        System.out.println(checkWhetherStringIsUnique("Competitive Programming"));
+    }
+
+    public static boolean checkWhetherStringIsUnique(String string) {
+        Map<Character, Boolean> characters = new HashMap<>();
+
+        // Write your code here...
+
+        return true;
+    }
 }
 
