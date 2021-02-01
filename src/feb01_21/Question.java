2f6d27ebc777ed4c78ab7301368b9d59c9f9c74e
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-01 10:25:57 +0530

    create Question.java

---
+++ src/feb01_21/Question.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 01-Feb-21
+ *   Time: 10:25 AM
+ *   File: Question.java
+ */
+
+package feb01_21;
+
+public class Question {
+}
+
