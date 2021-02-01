5bdb19c96d725544f7062f8812bf4de792a383cf
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-01 11:09:35 +0530

    final commit

--- src/feb01_21/Two.java
+++ src/feb01_21/Two.java
@@ -7,6 +7,40 @@
 
 package feb01_21;
 
+import java.util.Arrays;
+
 public class Two {
+    public static void main(String[] args) {
+        int index = 5;
+        int value = 55;
+        int[] a = {1, 2, 3, 4, 5};
+        int[] temp = insert(a, index, value);
+        System.out.println(Arrays.toString(temp));
+    }
+
+    public static int[] insert(int[] a, int index, int value) {
+        int temp[] = new int[a.length + 1];
+        if (a.length <= index) {
+            for (int i = 0; i < a.length; i++) {
+                temp[i] = a[i];
+            }
+            temp[a.length] = value;
+            return temp;
+        } else {
+            int i = 0;
+            int k = 0;
+            while (i <= a.length) {
+                if (i == index) {
+                    temp[i] = value;
+                    k -= 1;
+                } else {
+                    temp[i] = a[k];
+                }
+                i += 1;
+                k += 1;
+            }
+        }
+        return temp;
+    }
 }
 
