925c3184c1d70dd13a0d6fa650638cb1ed63cc7e
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-01 10:33:26 +0530

    add some question to Question.java

--- src/feb01_21/Question.java
+++ src/feb01_21/Question.java
@@ -6,7 +6,14 @@
  */
 
 package feb01_21;
-
+/*
+ * write a program to find largest and smallest value in array
+ * write a program to insert an element on specific into an array without overriding the value
+ * write a program to print the n numbers by using the recursion
+ * Create a class with name student having five attribute in the class StuRollno, StuName, Subject1,Subject2, Subject3
+ * Create an array of of ten students initialie each and every object of the class and then find out the result in how many subject
+ * of every student in how many subject the student is fail and you have to preserve the information into the array
+ *  */
 public class Question {
 }
 
