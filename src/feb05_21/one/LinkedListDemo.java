9d90d5ea2a67c2ab69e47a9cd0aa1f47a0c59fed
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-05 14:33:02 +0530

    add implementation of add and remove in LinkedListDemo.java

--- src/feb05_21/one/LinkedListDemo.java
+++ src/feb05_21/one/LinkedListDemo.java
@@ -7,8 +7,33 @@
 
 package feb05_21.one;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class LinkedListDemo {
     public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        // 5 operations:---
+        //1. Insert
+        for (int i = 0; i < 10; i++) {
+            list.add(
+                    new Random().nextInt(100) % 101
+            );
+        }
+        System.out.println(list);
+        var list1 = List.of(1, 2, 3);
+        list.addAll(list1);
+        System.out.println(list);
+        //2. Delete
+        list.remove();
+        System.out.println(list);
+        list.removeIf(n -> (n % 2 == 0));
+        System.out.println(list);
+        //3. Search
+        //4. Sort
+        //5. Traverse
+
 
     }
 }
