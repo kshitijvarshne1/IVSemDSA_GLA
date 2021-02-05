2102d9a0566ff8134acba54cd76d5337b8a60764
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-05 13:53:24 +0530

    create LinkedListDemo.java

---
+++ src/feb05_21/one/LinkedListDemo.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 05-Feb-21
+ *   Time: 1:53 PM
+ *   File: LinkedListDemo.java
+ */
+
+package feb05_21.one;
+
+public class LinkedListDemo {
+}
+
