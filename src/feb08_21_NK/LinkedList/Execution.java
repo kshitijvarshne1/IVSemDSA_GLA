acc7745260e928ee6df66910bb231d28a4f613e9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-09 00:02:36 +0530

    add merge method to Execution.java

--- src/feb08_21_NK/LinkedList/Execution.java
+++ src/feb08_21_NK/LinkedList/Execution.java
@@ -10,12 +10,38 @@ package feb08_21_NK.LinkedList;
 
 public class Execution {
     public static void main(String[] args) {
-        SinglyLinkedList list = new SinglyLinkedList();
-        for (int i = 0; i < 5; i++) {
-            list.insertAtBegin(new Node(i));
+        SinglyLinkedList list1 = new SinglyLinkedList();
+        SinglyLinkedList list2 = new SinglyLinkedList();
+        for (int i = 20; i >= 0; i -= 4) {
+            list1.insertAtBegin(new Node(i));
         }
-       list.printSinglyLinkedList();
+        for (int i = 19; i >= 0; i -= 3) {
+            list2.insertAtBegin(new Node(i));
+        }
+        SinglyLinkedList list3 = new SinglyLinkedList();
+        list3.head = merge(list1.head, list2.head);
+        list3.printSinglyLinkedList();
+
     }
+
+    public static Node merge(Node a, Node b) {
+        if (a == null) {
+            return b;
+        }
+        if (b == null) {
+            return a;
+        }
+        Node c;
+        if (a.data < b.data) {
+            c = a;
+            c.next = merge(a.next, b);
+        } else {
+            c = b;
+            c.next = merge(a, b.next);
+        }
+        return c;
+    }
+
 
 }
 
