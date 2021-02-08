684d46ea2192f76bd63455038268b9f413edb81a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-08 10:56:10 +0530

    create Node.java

---
+++ src/feb08_21_NK/LinkedList/Node.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 08-Feb-21
+ *   Time: 10:55 AM
+ *   File: Node.java
+ */
+
+package feb08_21_NK.LinkedList;
+
+public class Node {
+}
+
