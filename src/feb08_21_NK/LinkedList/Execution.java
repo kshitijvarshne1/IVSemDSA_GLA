5d2f0738e12227830ef40a8c30d6d3e936ec56de
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-08 11:13:54 +0530

    add some implementation for add and print the linked list

--- src/feb08_21_NK/LinkedList/Execution.java
+++ src/feb08_21_NK/LinkedList/Execution.java
@@ -10,7 +10,12 @@ package feb08_21_NK.LinkedList;
 
 public class Execution {
     public static void main(String[] args) {
-
+        SinglyLinkedList list = new SinglyLinkedList();
+        Node n1 = new Node(1);
+        list.insertAtBegin(n1);
+        Node n2 = new Node(2);
+        list.insertAtBegin(n2);
+        list.printSinglyLinkedList();
     }
 
 }
