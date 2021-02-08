26ba98d6e9f9c935dcb706900056e761aa5b782b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-08 11:40:30 +0530

    final commit

--- src/feb08_21_NK/LinkedList/Execution.java
+++ src/feb08_21_NK/LinkedList/Execution.java
@@ -11,10 +11,12 @@ package feb08_21_NK.LinkedList;
 public class Execution {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
-        Node n1 = new Node(1);
-        list.insertAtBegin(n1);
-        Node n2 = new Node(2);
-        list.insertAtBegin(n2);
+        for (int i = 0; i < 5; i++) {
+            list.insertAtBegin(new Node(i));
+        }
+        list.printSinglyLinkedList();
+        Node n1 = new Node(1000);
+        list.insertAtEnd(n1);
         list.printSinglyLinkedList();
     }
 
