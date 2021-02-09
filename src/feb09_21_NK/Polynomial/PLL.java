3acaaa90ad5992bf865273d0f9b3557516c6dafe
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-09 20:57:45 +0530

    add insert method to PLL.java

--- src/feb09_21_NK/Polynomial/PLL.java
+++ src/feb09_21_NK/Polynomial/PLL.java
@@ -8,5 +8,23 @@
 package feb09_21_NK.Polynomial;
 
 public class PLL {
+    Node head;
+
+    public PLL() {
+        head = null;
+    }
+
+    public void insert(Node newNode) {
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return;
+    }
 }
 
