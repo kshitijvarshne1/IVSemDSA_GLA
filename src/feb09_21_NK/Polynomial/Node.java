60af01e586827e7b08aae6d8925b502bc8908eb4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-09 20:50:33 +0530

    create Node.java

---
+++ src/feb09_21_NK/Polynomial/Node.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 09-Feb-21
+ *   Time: 8:49 PM
+ *   File: Node.java
+ */
+
+package feb09_21_NK.Polynomial;
+
+public class Node {
+}
