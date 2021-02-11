d7641d4642bf8a27aad5b98f43c8301b8a5d2c23
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-11 18:23:04 +0530

    add general method of stack in StackUsingArray.java

--- src/feb11_21/feb11_21_NK/one/StackUsingArray.java
+++ src/feb11_21/feb11_21_NK/one/StackUsingArray.java
@@ -8,5 +8,50 @@
 package feb11_21.feb11_21_NK.one;
 
 public class StackUsingArray {
+    int[] arr;
+    int top;
+
+    public StackUsingArray(int size) {
+        arr = new int[size];
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return top != arr.length - 1;
+    }
+
+    public boolean isFull() {
+        return top == arr.length;
+    }
+
+    public void push(int value) {
+        if (isEmpty()) {
+            top += 1;
+            arr[top] = value;
+            return;
+        }
+        System.out.println("Stack is full");
+        return;
+    }
+
+    public int top() {
+        if (!isEmpty()) {
+            return arr[top];
+        }
+        return -1;
+    }
+
+    public void pop() {
+        if (!isEmpty()) {
+            top -= 1;
+            return;
+        }
+        System.out.println("Underflow");
+        return;
+    }
+
+    public void deleteStack() {
+        arr = null;
+    }
 }
 
