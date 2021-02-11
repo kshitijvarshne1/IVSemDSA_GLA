d309d63c80a00928df5fcf5bb2517105d2557655
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-11 15:29:24 +0530

    create StackDemo.java

---
+++ src/feb11_21/one/StackDemo.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Feb-21
+ *   Time: 3:29 PM
+ *   File: StackDemo.java
+ */
+
+package feb11_21.one;
+
+public class StackDemo {
+}
+
