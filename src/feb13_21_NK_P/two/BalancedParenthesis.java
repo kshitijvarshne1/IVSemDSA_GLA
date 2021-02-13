f0ac32fd5d8f1f6f76726d44be25c4818d04ea99
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-13 19:15:35 +0530

    implement the logic of Balanced Parenthesis ->'('

--- src/feb13_21_NK_P/two/BalancedParenthesis.java
+++ src/feb13_21_NK_P/two/BalancedParenthesis.java
@@ -7,6 +7,31 @@
 
 package feb13_21_NK_P.two;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class BalancedParenthesis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String parenthesis = sc.nextLine();
+        System.out.println(check(parenthesis));
+
+    }
+
+    private static boolean check(String parenthesis) {
+        Stack<Character> stack = new Stack();
+        for (int i = 0; i < parenthesis.length(); i++) {
+            if (parenthesis.charAt(i) == '(') {
+                stack.push('(');
+            } else if (parenthesis.charAt(i) == ')') {
+                if (!stack.empty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
 }
 
