51039c81a0ca9d9205358294b6b6f1083559fba0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-13 15:20:01 +0530

    create first.java

---
+++ src/feb13_21_NK_P/Reverse_The_Stack/first.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-Feb-21
+ *   Time: 3:19 PM
+ *   File: first.java
+ */
+
+package feb13_21_NK_P.Reverse_The_Stack;
+
+public class first {
+}
+
