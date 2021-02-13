6f68b16f86dbb8045f6f888a82556df497a66464
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-13 15:40:26 +0530

    implement the logic for reverse the stack using 2 stack

--- src/feb13_21_NK_P/Reverse_The_Stack/first.java
+++ src/feb13_21_NK_P/Reverse_The_Stack/first.java
@@ -7,6 +7,38 @@
 
 package feb13_21_NK_P.Reverse_The_Stack;
 
+import java.util.Stack;
+
 public class first {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        for (int i = 0; i < 50; i++) {
+            stack.push(i);
+        }
+        Stack stack1 = new Stack();
+        while (!stack.empty()) {
+            System.out.print(stack.peek() + " -> ");
+            stack1.push(stack.peek());
+            stack.pop();
+        }
+        Stack stack2 = new Stack();
+        while (!stack1.empty()) {
+            stack2.push(stack1.peek());
+            stack1.pop();
+        }
+        while (!stack2.empty()) {
+            stack.push(stack2.peek());
+            stack2.pop();
+        }
+        System.out.println("Reverse the stack using 2 stack");
+        while (!stack.empty()) {
+            System.out.print(stack.peek() + " -> ");
+            stack.pop();
+        }
+
+    }
 }
 
+/*
+ * Reverse the stack using 2 stack
+ * */
