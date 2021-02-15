bd613a03ac7185fe2a154534b325ca4dcb1eec38
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-15 10:42:23 +0530

    implement the method of stack using linked list

--- src/feb15_21_NK/one/Execution.java
+++ src/feb15_21_NK/one/Execution.java
@@ -9,6 +9,15 @@ package feb15_21_NK.one;
 
 public class Execution {
     public static void main(String[] args) {
+        StackUsingLinkedList stack = new StackUsingLinkedList();
+        System.out.println(stack.isEmpty());
+        stack.push(new Node(5));
+        System.out.println(stack.isEmpty());
+        stack.peek();
+        stack.push(new Node(6));
+        stack.peek();
+        stack.pop();
+        stack.peek();
 
     }
 }
