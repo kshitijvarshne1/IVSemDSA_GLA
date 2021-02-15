254aa86739561457fd931d8d5def3b6bd9e016a4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-15 10:36:05 +0530

    add main method to Execution.java

---
+++ src/feb15_21_NK/one/Execution.java
@@ -0,0 +1,15 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 15-Feb-21
+ *   Time: 10:35 AM
+ *   File: Execution.java
+ */
+
+package feb15_21_NK.one;
+
+public class Execution {
+    public static void main(String[] args) {
+
+    }
+}
+
