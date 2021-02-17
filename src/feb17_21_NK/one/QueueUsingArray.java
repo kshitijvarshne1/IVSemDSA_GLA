a44ab6eaedff554a80ec9ccfc5f10d76611e3e68
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-17 19:39:38 +0530

    implements the methods isEmpty and isFull to QueueUsingArray.java

--- src/feb17_21_NK/one/QueueUsingArray.java
+++ src/feb17_21_NK/one/QueueUsingArray.java
@@ -8,5 +8,24 @@
 package feb17_21_NK.one;
 
 public class QueueUsingArray {
+    int[] arr;
+    int front;
+    int rear;
+    int size;
+
+    public QueueUsingArray(int size) {
+        arr = new int[size];
+        this.front = 0;
+        this.rear = 0;
+        this.size = size;
+    }
+
+    public boolean isEmpty() {
+        return rear == 0;
+    }
+
+    public boolean isFull() {
+        return rear == size - 1;
+    }
 }
 
