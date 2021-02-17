a1babcc0cc37759085617438bb726c3c988f0932
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-18 00:48:17 +0530

    final commit

--- src/feb17_21_NK/two/Execution.java
+++ src/feb17_21_NK/two/Execution.java
@@ -8,5 +8,16 @@
 package feb17_21_NK.two;
 
 public class Execution {
+    public static void main(String[] args) {
+        QueueUsingLinkedList list = new QueueUsingLinkedList();
+        for (int i = 1; i <= 5; i++) {
+            list.enqueue(new Node(i));
+        }
+        list.print();
+        for (int i = 0; i < 5; i++) {
+            list.dequeue();
+        }
+        list.dequeue();
+    }
 }
 
