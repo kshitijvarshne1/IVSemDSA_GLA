806a04a47b08adfdacd688ec621414f6d918128f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-17 19:56:40 +0530

    verify the logic and execute the method in Execution.java

--- src/feb17_21_NK/one/Execution.java
+++ src/feb17_21_NK/one/Execution.java
@@ -8,5 +8,19 @@
 package feb17_21_NK.one;
 
 public class Execution {
+    public static void main(String[] args) {
+        QueueUsingArray list = new QueueUsingArray(5);
+        for (int i = 1; i <= 5; i++) {
+            list.enqueue(i);
+        }
+        System.out.println(list.isFull());
+        list.dequeue();
+        list.dequeue();
+        list.dequeue();
+
+        list.dequeue();
+        list.dequeue();
+        list.dequeue();
+    }
 }
 
