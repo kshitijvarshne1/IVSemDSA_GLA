369532c18fa53e83082b35cf3179806b4f9600b7
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-18 00:40:10 +0530

    final commit

--- src/feb11_21/feb11_21_NK/one/Execution.java
+++ src/feb11_21/feb11_21_NK/one/Execution.java
@@ -10,18 +10,20 @@ package feb11_21.feb11_21_NK.one;
 public class Execution {
     public static void main(String[] args) {
         StackUsingArray stack = new StackUsingArray(5);
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <= 5; i++) {
             stack.push(i * i);
         }
-        stack.push(101);
-        System.out.println(stack.isEmpty());
-        System.out.println(stack.isFull());
-        stack.push(99);
-        stack.pop();
-        stack.push(444);
-        stack.pop();
-        System.out.println(stack.isEmpty());
         System.out.println(stack.peek());
-        stack.deleteStack();
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.peek());
     }
 }
