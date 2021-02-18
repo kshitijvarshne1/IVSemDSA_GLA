a0ac2c58db34b5d5daec337bb1f831ef661d5b74
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-18 17:03:00 +0530

    add method converter to convert morse code

--- src/feb18_21/one/MorseCode.java
+++ src/feb18_21/one/MorseCode.java
@@ -8,5 +8,21 @@
 package feb18_21.one;
 
 public class MorseCode {
+    public static void main(String[] args) {
+        System.out.println(converter("CANTEEN"));
+    }
+
+    private static StringBuilder converter(String s) {
+        String[] morseCode = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+                ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int k = c - 'A';
+            result.append(morseCode[k]);
+        }
+        return result;
+    }
+
 }
 
