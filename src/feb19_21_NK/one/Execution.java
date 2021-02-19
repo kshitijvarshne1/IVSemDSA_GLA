eb44b33342e02c8a74b658e087c81e672601543d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-19 20:33:11 +0530

    add implements the methods of enqueue,dequeue, isFull and isEmpty

--- src/feb19_21_NK/one/Execution.java
+++ src/feb19_21_NK/one/Execution.java
@@ -8,5 +8,32 @@
 package feb19_21_NK.one;
 
 public class Execution {
+    public static void main(String[] args) {
+        CircularQueueUsingArray list = new CircularQueueUsingArray(5);
+        System.out.println(list.isEmpty());
+        System.out.println(list.isFull());
+        list.dequeue();
+        list.enqueue(1);
+        System.out.println(list.isEmpty());
+        System.out.println(list.isFull());
+        list.enqueue(2);
+        System.out.println(list.isEmpty());
+        System.out.println(list.isFull());
+        list.enqueue(3);
+        System.out.println(list.isEmpty());
+        System.out.println(list.isFull());
+        list.enqueue(4);
+        System.out.println(list.isEmpty());
+        System.out.println(list.isFull());
+        list.enqueue(5);
+        System.out.println("Hello");
+        System.out.println(list.isEmpty());
+        System.out.println(list.isFull());
+        list.dequeue();
+        list.dequeue();
+        list.dequeue();
+        System.out.println(list.isEmpty());
+
+    }
 }
 
