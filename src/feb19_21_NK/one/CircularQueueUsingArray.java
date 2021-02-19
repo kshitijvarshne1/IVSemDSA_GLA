c76dbe83629bd1ba6ebc95c29735e40ef28de1cd
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-19 20:08:58 +0530

    add enqueue and isFull method

--- src/feb19_21_NK/one/CircularQueueUsingArray.java
+++ src/feb19_21_NK/one/CircularQueueUsingArray.java
@@ -8,5 +8,30 @@
 package feb19_21_NK.one;
 
 public class CircularQueueUsingArray {
+    public int[] arr;
+    public int front;
+    public int rear;
+
+    public CircularQueueUsingArray(int size) {
+        arr = new int[size];
+        this.front = -1;
+        this.rear = -1;
+    }
+
+    public void enqueue(int data) {
+        if (front == -1 && rear == -1) {
+            front = rear = 0;
+            arr[front] = data;
+        } else if (!isFull()) {
+            rear = (rear + 1) % arr.length;
+            arr[rear] = data;
+        } else {
+            System.out.println("Queue is full");
+        }
+    }
+
+    public boolean isFull() {
+        return (rear + 1) % arr.length == front;
+    }
 }
 
