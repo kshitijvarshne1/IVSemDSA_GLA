87c975dae06da449de26f6da14785a80e855ff16
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-24 20:16:34 +0530

    implement the method of DoublyEndedQueue

--- src/February/feb24_21_NK/Execution.java
+++ src/February/feb24_21_NK/Execution.java
@@ -8,5 +8,21 @@
 package February.feb24_21_NK;
 
 public class Execution {
+    public static void main(String[] args) {
+        DoublyEndedQueue list = new DoublyEndedQueue();
+        for (int i = 0; i < 5; i++) {
+            list.enqueueAtEnd(new Node(i));
+        }
+        list.print();
+        list.enqueueAtFront(new Node(22));
+        list.print();
+        list.dequeueAtEnd();
+        list.print();
+        list.dequeueAtFirst();
+        list.print();
+        System.out.println(list.peek());
+        System.out.println(list.peekEnd());
+
+    }
 }
 
