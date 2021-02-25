e64c070c2da1e742958081b6d510b441ccee3e6f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-25 15:40:43 +0530

    implements the methods of PriorityQueueUsingLinkedList.java

--- src/February/feb_21_NK/Execution.java
+++ src/February/feb_21_NK/Execution.java
@@ -8,5 +8,21 @@
 package February.feb_21_NK;
 
 public class Execution {
+    public static void main(String[] args) {
+        PriorityQueueUsingLinkedList list = new PriorityQueueUsingLinkedList();
+        list.enqueue(new Node(5, 2));
+        list.print();
+        list.enqueue(new Node(4, 1));
+        list.print();
+        list.enqueue(new Node(5, 3));
+        list.print();
+        list.enqueue(new Node(55, 2));
+        list.print();
+        System.out.println(list.peek());
+        list.dequeue();
+        System.out.println(list.peek());
+        list.print();
+
+    }
 }
 
