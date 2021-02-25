3a5353638561e9250999aaddd70de318490ab7d8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-25 15:28:39 +0530

    add enqueue and print method

--- src/February/feb_21_NK/PriorityQueueUsingLinkedList.java
+++ src/February/feb_21_NK/PriorityQueueUsingLinkedList.java
@@ -14,5 +14,35 @@ public class PriorityQueueUsingLinkedList {
         this.head = null;
     }
 
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void dequeue(Node newNode) {
+        if (!isEmpty() && newNode.priority < head.priority) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.priority <= newNode.priority) {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "{ " + temp.priority + " }" + " -> ");
+            }
+            System.out.println();
+            return;
+        }
+        System.out.println("Queue is empty");
+    }
+
 }
 
