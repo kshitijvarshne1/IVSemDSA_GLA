18fa151b4a5cc1ee7649afe8179c8e80c1877d16
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-27 17:11:36 +0530

    implemets the methods of PriorityQueue

--- src/February/feb27_21_NK/PriorityQueueDemo1.java
+++ src/February/feb27_21_NK/PriorityQueueDemo1.java
@@ -7,6 +7,26 @@
 
 package February.feb27_21_NK;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class PriorityQueueDemo1 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        pq.add(1);
+        pq.add(4);
+        pq.add(2);
+        pq.add(3);
+        Iterator iterator = pq.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+        System.out.println(pq.peek());
+        System.out.println(pq.poll());
+        System.out.println(pq.peek());
+
+    }
 }
 
