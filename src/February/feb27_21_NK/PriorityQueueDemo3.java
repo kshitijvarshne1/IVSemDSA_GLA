248f80d870dab03e3951fc6391adda487277f371
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-27 19:12:34 +0530

    implements the methods of PriorityQueue using lambdaExpression to create Lambda Expression

--- src/February/feb27_21_NK/PriorityQueueDemo3.java
+++ src/February/feb27_21_NK/PriorityQueueDemo3.java
@@ -7,6 +7,31 @@
 
 package February.feb27_21_NK;
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class PriorityQueueDemo3 {
+    public static void main(String[] args) {
+        //Using Lambda Expression
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        pq.add(1);
+        pq.add(5);
+        pq.add(2);
+        pq.add(4);
+        pq.add(3);
+        Iterator iterator = pq.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+
+
+    }
 }
 
