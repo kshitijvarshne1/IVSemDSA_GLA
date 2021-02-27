0565b7ba809a10cbdfe5fd5d66baf1efb19cf0b2
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-27 17:01:56 +0530

    implements the methods of PriorityQueue

--- src/February/feb27_21_NK/PriorityQueueDemo.java
+++ src/February/feb27_21_NK/PriorityQueueDemo.java
@@ -7,9 +7,21 @@
 
 package February.feb27_21_NK;
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class PriorityQueueDemo {
     public static void main(String[] args) {
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(1);
+        pq.add(3);
+        pq.add(2);
+        Iterator iterator = pq.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+        System.out.println(pq.peek());
     }
 }
 
