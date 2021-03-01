73701003e719c2b532d9e973ce18eef62a6d6a73
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-01 11:01:29 +0530

    final commit

--- src/March/Base/StorageDetail.java
+++ src/March/Base/StorageDetail.java
@@ -8,5 +8,43 @@
 package March.Base;
 
 public class StorageDetail {
+    public Node head;
+
+    public StorageDetail() {
+        this.head = null;
+    }
+
+    public void enqueue(Node newNode) {
+        if (isEmpty() || newNode.data.getSalary() > head.data.getSalary()) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data.getSalary() >= newNode.data.getSalary()) {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node temp = head;
+            while (temp != null) {
+                System.out.println(temp.data.getName() + " " + temp.data.getDesignation() + " " + temp.data.getSalary() + " " + temp.data.getId());
+                temp = temp.next;
+            }
+            System.out.println();
+            return;
+        }
+        System.out.println("Empty");
+    }
+
 }
 
