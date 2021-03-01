74d9068eb493a04fd3be144e35e501f4aff9cfe8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-01 10:23:42 +0530

    add constructor , getter and setter

--- src/March/Base/Employee.java
+++ src/March/Base/Employee.java
@@ -8,5 +8,49 @@
 package March.Base;
 
 public class Employee {
+    private String name;
+    private String designation;
+    private int salary;
+    private int id;
+
+    public Employee(String name, String designation, int salary, int id) {
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
 
