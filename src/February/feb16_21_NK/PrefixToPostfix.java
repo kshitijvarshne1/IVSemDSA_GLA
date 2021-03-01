fbb0719caa452915e199d90cd4a16980100ccf93
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-01 17:44:49 +0530

    add code to PrefixToPostfix.java

--- src/February/feb16_21_NK/PrefixToPostfix.java
+++ src/February/feb16_21_NK/PrefixToPostfix.java
@@ -7,6 +7,46 @@
 
 package February.feb16_21_NK;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class PrefixToPostfix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String prefix = sc.next();
+        System.out.println("Prefix:- " + prefix);
+        System.out.println("Postfix:- " + convertPrefixToPostfix(prefix));
+
+    }
+
+    private static String convertPrefixToPostfix(String prefix) {
+        Stack<String> stack = new Stack();
+        for (int i = prefix.length() - 1; i >= 0; i--) {
+            if (isOperator(prefix.charAt(i))) {
+                String op1 = stack.peek();
+                stack.pop();
+                String op2 = stack.peek();
+                stack.pop();
+                String temp = op1 + op2 + prefix.charAt(i);
+                stack.push(temp);
+            } else {
+                stack.push(prefix.charAt(i) + "");
+            }
+        }
+        return stack.peek();
+    }
+
+    public static boolean isOperator(char x) {
+        switch (x) {
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+            case '^':
+                return true;
+        }
+        return false;
+    }
+
 }
 
