0b762fdcf7185c967105ff471b6bf170e7d47818
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-01 11:01:42 +0530

    final commit

--- src/March/StringOne.java
+++ src/March/StringOne.java
@@ -7,6 +7,26 @@
 
 package March;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class StringOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] arr = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            char temp = str.charAt(i);
+            arr[i] = (int) temp;
+        }
+        Arrays.sort(arr);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+
+            result.append((char) arr[i]);
+        }
+        System.out.println(result);
+
+    }
 }
 
