2cecb021fd23db5f7890b52d9340e9d2c774564b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-01 10:21:50 +0530

    create Employee.java

---
+++ src/March/Base/Employee.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 01-Mar-21
+ *   Time: 10:21 AM
+ *   File: Employee.java
+ */
+
+package March.Base;
+
+public class Employee {
+}
+
