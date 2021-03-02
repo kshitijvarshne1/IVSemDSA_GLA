bff0dec866b862b48b2d03a7ea1e9787e239e338
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-02 20:04:48 +0530

    implements the SinglyLinkedList class

--- src/March/mar02_21/one/Execution.java
+++ src/March/mar02_21/one/Execution.java
@@ -8,5 +8,16 @@
 package March.mar02_21.one;
 
 public class Execution {
+    public static void main(String[] args) {
+        SinglyLinkedList ll = new SinglyLinkedList();
+        //ll.insertAtBegin(new Node(5));
+        //ll.insertAtBegin(new Node(4));
+        ll.insertAtEnd(new Node(6));
+        ll.insertAtBegin(new Node(2));
+        ll.print();
+        ll.deleteAtBegin();
+        ll.print();
+    }
+
 }
 
