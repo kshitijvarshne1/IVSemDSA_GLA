f7648b3a0d65336c981eaf6452f8a38eefbe353f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-03 02:02:06 +0530

    implements the StackUsingArray class

--- src/February/feb09_21_NK/two/Main.java
+++ src/February/feb09_21_NK/two/Main.java
@@ -8,5 +8,22 @@
 package February.feb09_21_NK.two;
 
 public class Main {
+    public static void main(String[] args) {
+        StackUsingArray st = new StackUsingArray(5);
+        st.push(5);
+        System.out.println(st.peek());
+        System.out.println(st.isEmpty());
+        st.push(56);
+        st.push(4);
+        st.push(52);
+        st.push(51);
+        System.out.println(st.peek());
+        System.out.println(st.isEmpty());
+        System.out.println(st.isFull());
+        st.pop();
+        System.out.println(st.isFull());
+        System.out.println(st.isEmpty());
+
+    }
 }
 
