91a851c3666c2875a7f2d30c56161a68ecec2a3f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-02 20:07:08 +0530

    add fields and constructor

--- src/March/mar02_21/two/Node.java
+++ src/March/mar02_21/two/Node.java
@@ -8,5 +8,15 @@
 package March.mar02_21.two;
 
 public class Node {
+    public int data;
+    public Node next;
+    public Node prev;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
 }
 
