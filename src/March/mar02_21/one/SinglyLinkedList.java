109fedaff9a176b8126da47fa42bf4d71eadf468
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-02 20:00:11 +0530

    create insertAtEnd, insertAtBegin , deleteAtEnd, print and isEmpty method

--- src/March/mar02_21/one/SinglyLinkedList.java
+++ src/March/mar02_21/one/SinglyLinkedList.java
@@ -8,5 +8,64 @@
 package March.mar02_21.one;
 
 public class SinglyLinkedList {
+    public Node head;
+
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    public void insertAtEnd(Node newNode) {
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void insertAtBegin(Node newNode) {
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            head = newNode;
+            newNode.next = temp;
+        }
+
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void deleteAtEnd() {
+        if (!isEmpty() && head.next == null) {
+            head = null;
+        } else if (!isEmpty()) {
+            Node temp = head;
+            while (temp.next.next != null) {
+                temp = temp.next;
+            }
+            temp.next = null;
+        } else {
+            System.out.println("empty");
+        }
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+            System.out.println();
+        } else {
+            System.out.println("Empty");
+        }
+    }
 }
 
