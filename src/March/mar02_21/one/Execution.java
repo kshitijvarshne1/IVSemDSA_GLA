84c711db00b3eacc0f5f5a54826bf2edeb770c0a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-03 22:09:39 +0530

    final commit

--- src/March/mar02_21/one/Execution.java
+++ src/March/mar02_21/one/Execution.java
@@ -10,12 +10,13 @@ package March.mar02_21.one;
 public class Execution {
     public static void main(String[] args) {
         SinglyLinkedList ll = new SinglyLinkedList();
-        //ll.insertAtBegin(new Node(5));
-        //ll.insertAtBegin(new Node(4));
+        ll.insertAtEnd(new Node(5));
+        ll.insertAtBegin(new Node(4));
         ll.insertAtEnd(new Node(6));
-        ll.insertAtBegin(new Node(2));
+        ll.insertAtEnd(new Node(2));
         ll.print();
-        ll.deleteAtBegin();
+        ll.sorting();
+        ;
         ll.print();
     }
 }
