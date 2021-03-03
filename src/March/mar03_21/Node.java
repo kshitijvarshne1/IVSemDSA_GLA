930d7c38bd6c6e436f3da973177826d553be1b54
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-03 22:13:11 +0530

    create Node.java

---
+++ src/March/mar03_21/Node.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 03-Mar-21
+ *   Time: 10:12 PM
+ *   File: Node.java
+ */
+
+package March.mar03_21;
+
+public class Node {
+}
+
