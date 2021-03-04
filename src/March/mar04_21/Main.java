93143ec960f6fcd1a528b0450b27187183ec5935
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-04 20:36:52 +0530

    implements the method of StackUsingArray

--- src/March/mar04_21/Main.java
+++ src/March/mar04_21/Main.java
@@ -8,5 +8,21 @@
 package March.mar04_21;
 
 public class Main {
+    public static void main(String[] args) {
+        StackUsingArray st = new StackUsingArray(5);
+        System.out.println(st.isEmpty());
+        st.push(1);
+        System.out.println(st.isEmpty());
+        System.out.println(st.isFull());
+        st.peek();
+        st.push(4);
+        st.push(5);
+        st.push(6);
+        st.push(8);
+        System.out.println(st.isEmpty());
+        System.out.println(st.isFull());
+        st.pop();
+        System.out.println(st.isFull());
+    }
 }
 
