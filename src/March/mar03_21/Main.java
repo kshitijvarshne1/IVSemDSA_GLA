509c7c77921456107512bfad95db55474d945cc6
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-04 20:15:54 +0530

    add insertAtEnd method

--- src/March/mar03_21/Main.java
+++ src/March/mar03_21/Main.java
@@ -12,7 +12,9 @@ public class Main {
         CircularLinkedList ll = new CircularLinkedList();
         ll.insertAtEnd(new Node(5));
         ll.insertAtEnd(new Node(6));
-        ll.insertAtBegin(new Node(4));
+        ll.insertAtEnd(new Node(66));
+        ll.insertAtBegin(new Node(3));
+        ll.insertAtBegin(new Node(9));
         ll.print();
     }
 }
