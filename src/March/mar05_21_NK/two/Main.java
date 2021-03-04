819c3a3c16205a97a60b5243919e3f9ae77506c2
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-05 02:25:27 +0530

    implements the methods of CircularQueueusingLinkedList

--- src/March/mar05_21_NK/two/Main.java
+++ src/March/mar05_21_NK/two/Main.java
@@ -7,6 +7,18 @@
 
 package March.mar05_21_NK.two;
 
+import March.mar03_21_NK.Node;
+
 public class Main {
+    public static void main(String[] args) {
+        CircularQueueUsingLinkedList cc = new CircularQueueUsingLinkedList();
+        cc.enqueue(new Node(3));
+        cc.peek();
+        cc.enqueue(new Node(4));
+        cc.dequeue();
+        cc.dequeue();
+        cc.dequeue();
+    }
+
 }
 
