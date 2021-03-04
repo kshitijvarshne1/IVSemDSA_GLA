f9ae6aa419f3c2f9185b6cf88931d5f1efc28906
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-04 20:24:16 +0530

    implements the deleteAtBegin method

--- src/March/mar03_21/Main.java
+++ src/March/mar03_21/Main.java
@@ -11,12 +11,9 @@ public class Main {
     public static void main(String[] args) {
         CircularLinkedList ll = new CircularLinkedList();
         ll.insertAtEnd(new Node(5));
-        ll.insertAtEnd(new Node(6));
-        ll.insertAtEnd(new Node(66));
-        ll.insertAtBegin(new Node(3));
-        ll.insertAtBegin(new Node(9));
         ll.print();
-        ll.deleteAtEnd();
+        ll.deleteAtBegin();
+
         ll.print();
 
     }
