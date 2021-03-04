c3f936954804dd2bd638db6fad429c85f7e1d05c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-04 23:12:00 +0530

    final commit

--- src/March/mar04_21_NK/four/QueueUsingLinkedList.java
+++ src/March/mar04_21_NK/four/QueueUsingLinkedList.java
@@ -8,5 +8,37 @@
 package March.mar04_21_NK.four;
 
 public class QueueUsingLinkedList {
+    public Node front;
+    public Node rear;
+
+    public QueueUsingLinkedList() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    public void enqueue(Node newNode) {
+        if (front == null && rear == null) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+    }
+
+    public void dequeue() {
+        if (front == null) {
+            System.out.println("Underflow");
+        } else if (front == rear && front != null) {
+            System.out.println(front.data);
+            front = rear = null;
+        } else {
+            System.out.println(front.data);
+            front = front.next;
+        }
+    }
+
+    public boolean isFull() {
+        return false;
+    }
 }
 
