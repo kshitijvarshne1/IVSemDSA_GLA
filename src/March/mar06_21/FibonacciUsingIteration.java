e15fc262421fc544ed48d3fea017910a19b2ae91
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-06 01:54:07 +0530

    final commit

--- src/March/mar06_21/FibonacciUsingIteration.java
+++ src/March/mar06_21/FibonacciUsingIteration.java
@@ -7,6 +7,21 @@
 
 package March.mar06_21;
 
+import java.util.Scanner;
+
 public class FibonacciUsingIteration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i <= n; i++) {
+            System.out.print(a + b + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
 }
 
