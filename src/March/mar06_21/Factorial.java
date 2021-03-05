516a26396028db23c5e45f08811ad6bb5b93f8e4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-06 01:33:04 +0530

    create Factorial.java

---
+++ src/March/mar06_21/Factorial.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 06-Mar-21
+ *   Time: 1:32 AM
+ *   File: Factorial.java
+ */
+
+package March.mar06_21;
+
+public class Factorial {
+}
+
