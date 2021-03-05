abed104f5d034e9d47e6fa50f6f77e61c62ee632
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-06 01:22:54 +0530

    add main method

--- src/March/mar06_21/TowerOfHanoi.java
+++ src/March/mar06_21/TowerOfHanoi.java
@@ -7,6 +7,12 @@
 
 package March.mar06_21;
 
+import java.util.Scanner;
+
 public class TowerOfHanoi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
 }
 
