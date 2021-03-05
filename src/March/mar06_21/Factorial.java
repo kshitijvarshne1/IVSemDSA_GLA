9bf5d3835e2fa9a4ef62f1b7e01d53a828ae33ea
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-06 01:35:37 +0530

    add factorial method

--- src/March/mar06_21/Factorial.java
+++ src/March/mar06_21/Factorial.java
@@ -7,6 +7,21 @@
 
 package March.mar06_21;
 
+import java.util.Scanner;
+
 public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(factorial(number));
+    }
+
+    private static int factorial(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
 }
 
