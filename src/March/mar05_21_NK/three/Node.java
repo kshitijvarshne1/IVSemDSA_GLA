a1838acb6cdf44d6adb03def05b090ba51582121
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-05 11:33:12 +0530

    final commit

--- src/March/mar05_21_NK/three/Node.java
+++ src/March/mar05_21_NK/three/Node.java
@@ -8,5 +8,14 @@
 package March.mar05_21_NK.three;
 
 public class Node {
+    public int data;
+    public int priority;
+    public Node next;
+
+    public Node(int data, int priority) {
+        this.data = data;
+        this.priority = priority;
+        this.next = null;
+    }
 }
 
