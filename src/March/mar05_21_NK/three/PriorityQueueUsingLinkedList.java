e894c597e1910ff4aeaf7005769d03476df7d922
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-05 11:37:58 +0530

    final commit

--- src/March/mar05_21_NK/three/PriorityQueueUsingLinkedList.java
+++ src/March/mar05_21_NK/three/PriorityQueueUsingLinkedList.java
@@ -8,5 +8,56 @@
 package March.mar05_21_NK.three;
 
 public class PriorityQueueUsingLinkedList {
+    public Node head;
+
+    public PriorityQueueUsingLinkedList() {
+        this.head = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void enqueue(Node newNode) {
+        if (isEmpty() || newNode.priority < head.priority) {
+            newNode.next = head.next;
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null && temp.next.priority <= newNode.priority) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " { " + temp.priority + " }" + " -> ");
+                temp = temp.next;
+            }
+            System.out.println();
+            return;
+        }
+        System.out.println("Queue is empty");
+    }
+
+    public void dequeue() {
+        if (!isEmpty()) {
+            head = head.next;
+            return;
+        }
+        System.out.println("Queue is empty");
+    }
+
+    public int peek() {
+        if (!isEmpty()) {
+            return head.data;
+        }
+        return -1;
+    }
 }
 
