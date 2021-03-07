3e64e0f268c9863c3da9cfd5b830dfa3df4738b6
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-07 20:55:48 +0530

    add code to FibUsingRecursion.java

--- src/March/mar07_21_NK/FibUsingRecursion.java
+++ src/March/mar07_21_NK/FibUsingRecursion.java
@@ -7,6 +7,26 @@
 
 package March.mar07_21_NK;
 
+import java.util.Scanner;
+
 public class FibUsingRecursion {
+    static int a = 0, b = 1;
+
+    public static void fib(int n) {
+        if (n > 0) {
+            int c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+            fib(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(a + " " + b);
+        fib(n - 2);
+    }
 }
 
