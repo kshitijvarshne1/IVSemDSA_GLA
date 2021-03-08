6f484c23dc80c696b8c81616a01e8f44685b7aac
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-08 22:50:19 +0530

    create Student.java

---
+++ src/March/mar08_21_NK/Student.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 08-Mar-21
+ *   Time: 10:50 PM
+ *   File: Student.java
+ */
+
+package March.mar08_21_NK;
+
+public class Student {
+}
+
