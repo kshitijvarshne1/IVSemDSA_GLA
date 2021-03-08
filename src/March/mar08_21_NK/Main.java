0d468bad088c5bb0e31a30d5b9f8ab023e214dc6
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-08 22:58:00 +0530

    final commit

--- src/March/mar08_21_NK/Main.java
+++ src/March/mar08_21_NK/Main.java
@@ -8,5 +8,13 @@
 package March.mar08_21_NK;
 
 public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList ll = new SinglyLinkedList();
+        ll.insertAtBegin(new Node(new Student("adi", 2)));
+        ll.insertAtEnd(new Node(new Student("di", 1)));
+        ll.print();
+        ll.sorting();
+        ll.print();
+    }
 }
 
