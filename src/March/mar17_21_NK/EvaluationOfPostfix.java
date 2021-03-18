f87cd2702ce5b9863f138b3e93a8ab3ca3141836
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-18 22:13:07 +0530

    add evaluation method

--- src/March/mar17_21_NK/EvaluationOfPostfix.java
+++ src/March/mar17_21_NK/EvaluationOfPostfix.java
@@ -8,12 +8,45 @@
 package March.mar17_21_NK;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class EvaluationOfPostfix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String postfix = sc.nextLine();
         // System.out.println("Postfix Evaluation :-  " + Evaluation(postfix));
+    }
+
+    private static int Evaluation(String postfix) {
+        Stack<Integer> stack = new Stack();
+        for (int i = 0; i < postfix.length(); i++) {
+            char c = postfix.charAt(i);
+            if (Character.isDigit(c)) {
+                stack.push(c - '0');
+            } else {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+
+                switch (c) {
+                    case '+':
+                        stack.push(val2 + val1);
+                        break;
+
+                    case '-':
+                        stack.push(val2 - val1);
+                        break;
+
+                    case '/':
+                        stack.push(val2 / val1);
+                        break;
+
+                    case '*':
+                        stack.push(val2 * val1);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
     }
 
 
