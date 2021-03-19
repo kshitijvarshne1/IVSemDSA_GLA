f65bbd58b33bc5f25e8cf93f3c1e5ee322c5ee5b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-20 04:31:00 +0530

    implement the concept of regular expression

--- src/March/mar21_21_NK/Sample.java
+++ src/March/mar21_21_NK/Sample.java
@@ -7,6 +7,14 @@
 
 package March.mar21_21_NK;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Sample {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("^\\d{4}[ ]\\d{4}[ ]\\d{4}$");
+        Matcher matcher = pattern.matcher("1234 5678 9876");
+        System.out.println(matcher.matches());
+    }
 }
 
