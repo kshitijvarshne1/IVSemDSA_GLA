d67e4a78f7ee691f07f5032cddbb3313c22a0fd0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-19 23:52:01 +0530

    implement some concept of ArrayList

--- src/March/mar20_21/one/Sample.java
+++ src/March/mar20_21/one/Sample.java
@@ -7,6 +7,16 @@
 
 package March.mar20_21.one;
 
+import java.util.ArrayList;
+
 public class Sample {
+    public static void main(String[] args) {
+        ArrayList ar = new ArrayList();
+        ar.add(1);
+        ar.add(2);
+        ar.add(1, 4);
+        ar.add(4);
+        System.out.println(ar.toString());
+    }
 }
 
