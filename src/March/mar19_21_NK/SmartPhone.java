189ed5c041c4637335e142c1988fc246bf3255da
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-19 16:14:44 +0530

    create fields and constructor

--- src/March/mar19_21_NK/SmartPhone.java
+++ src/March/mar19_21_NK/SmartPhone.java
@@ -8,5 +8,16 @@
 package March.mar19_21_NK;
 
 public class SmartPhone {
+    private final long IMEINumber;
+    private final String name;
+    private double price;
+    private byte screenSize;
+
+    public SmartPhone(long IMEINumber, String name, double price, byte screenSize) {
+        this.IMEINumber = IMEINumber;
+        this.name = name;
+        this.price = price;
+        this.screenSize = screenSize;
+    }
 }
 
