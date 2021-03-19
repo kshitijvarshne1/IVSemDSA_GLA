bff42b4b314c28112ee1131c14994f0f48a9e8ef
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-19 21:30:49 +0530

    create Sample.java

---
+++ src/March/mar20_21/one/Sample.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 19-Mar-21
+ *   Time: 9:30 PM
+ *   File: Sample.java
+ */
+
+package March.mar20_21.one;
+
+public class Sample {
+}
+
