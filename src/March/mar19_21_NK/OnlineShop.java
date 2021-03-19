9333735a70546be30f84d62c6209ee81cceaa290
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-19 16:25:18 +0530

    add getListOfSmartPhones method

--- src/March/mar19_21_NK/OnlineShop.java
+++ src/March/mar19_21_NK/OnlineShop.java
@@ -7,7 +7,22 @@
 
 package March.mar19_21_NK;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class OnlineShop {
 
+    public List<SmartPhone> getListOfSmartPhones(Set<SmartPhone> smartPhones, byte requiredScreenSize) {
+        List<SmartPhone> smartPhoneList = new ArrayList<>();
+        for (SmartPhone smartPhone : smartPhones) {
+            if (smartPhone.getScreenSize() >= requiredScreenSize) {
+                smartPhoneList.add(smartPhone);
+            } else {
+                smartPhone.setPrice(smartPhone.getPrice() - smartPhone.getPrice() * 0.1);
+            }
+        }
+        return smartPhoneList;
+    }
 }
 
