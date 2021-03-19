86487bfa6129f283cba80d8cec1b126dcc2a26af
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-20 02:13:59 +0530

    add fields , constructor and getter and setter methods

--- src/March/mar20_21/two/Person.java
+++ src/March/mar20_21/two/Person.java
@@ -8,5 +8,38 @@
 package March.mar20_21.two;
 
 public class Person {
+    private int id;
+    private String name;
+    private byte age;
+
+    public Person(int id, String name, byte age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
 }
 
