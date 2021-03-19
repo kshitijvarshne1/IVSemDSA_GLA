8ddaa5f07b14c1cd8ae5fdbdf527a9d0f35eb65f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-19 16:15:40 +0530

    add equals , hascode and toString method

--- src/March/mar19_21_NK/SmartPhone.java
+++ src/March/mar19_21_NK/SmartPhone.java
@@ -7,6 +7,8 @@
 
 package March.mar19_21_NK;
 
+import java.util.Objects;
+
 public class SmartPhone {
     private final long IMEINumber;
     private final String name;
@@ -19,5 +21,53 @@ public class SmartPhone {
         this.price = price;
         this.screenSize = screenSize;
     }
+
+    public long getIMEINumber() {
+        return IMEINumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public byte getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(byte screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SmartPhone that = (SmartPhone) o;
+        return getIMEINumber() == that.getIMEINumber() && Double.compare(that.getPrice(), getPrice()) == 0 && getScreenSize() == that.getScreenSize() && Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIMEINumber(), getName(), getPrice(), getScreenSize());
+    }
+
+    @Override
+    public String toString() {
+        return "SmartPhone{" +
+                "IMEINumber=" + IMEINumber +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", screenSize=" + screenSize +
+                '}';
+    }
+
 }
 
