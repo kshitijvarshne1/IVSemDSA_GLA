918dfe9a482b9b03955d0697156e72e3f635c72d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-19 16:09:40 +0530

    create SmartPhone.java

---
+++ src/March/mar19_21_NK/SmartPhone.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 19-Mar-21
+ *   Time: 4:09 PM
+ *   File: SmartPhone.java
+ */
+
+package March.mar19_21_NK;
+
+public class SmartPhone {
+}
+
