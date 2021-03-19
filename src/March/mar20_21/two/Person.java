b091933927f29e2464f78ca6903e4bf17da5a053
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-20 02:12:24 +0530

    create Person.java

---
+++ src/March/mar20_21/two/Person.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 20-Mar-21
+ *   Time: 2:12 AM
+ *   File: Person.java
+ */
+
+package March.mar20_21.two;
+
+public class Person {
+}
+
