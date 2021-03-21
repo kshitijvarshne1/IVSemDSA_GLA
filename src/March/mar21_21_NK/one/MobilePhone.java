97df9287e7644412c90ac8a385b29e684d587b14
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-21 22:39:48 +0530

    add constructor and sendTextMessage

--- src/March/mar21_21_NK/one/MobilePhone.java
+++ src/March/mar21_21_NK/one/MobilePhone.java
@@ -8,5 +8,23 @@
 package March.mar21_21_NK.one;
 
 public class MobilePhone {
+    public int balance;
+
+    public MobilePhone(int balance) {
+        this.balance = balance;
+    }
+
+    public void sendTextMessage(long recipientContactNumber, String message) throws LowBalanceException {
+        if (this.balance <= 1.5) {
+            throw new LowBalanceException("Balance is low");
+        } else {
+            getLoan();
+        }
+    }
+
+    public void getLoan() {
+        this.balance += 10;
+    }
+
 }
 
