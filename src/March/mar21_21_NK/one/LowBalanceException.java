0e7ac50e6a94890637c55c31b366ad52ec51142a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-21 22:35:15 +0530

    create LowBalanceException.java

---
+++ src/March/mar21_21_NK/one/LowBalanceException.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 21-Mar-21
+ *   Time: 10:34 PM
+ *   File: LowBalanceException.java
+ */
+
+package March.mar21_21_NK.one;
+
+public class LowBalanceException {
+}
+
