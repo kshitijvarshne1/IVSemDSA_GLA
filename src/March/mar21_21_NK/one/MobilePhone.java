480fa5b409f8a0b332781a166a19a62b92f55bf0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-21 22:33:13 +0530

    create MobilePhone.java

---
+++ src/March/mar21_21_NK/one/MobilePhone.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 21-Mar-21
+ *   Time: 10:32 PM
+ *   File: MobilePhone.java
+ */
+
+package March.mar21_21_NK.one;
+
+public class MobilePhone {
+}
+
