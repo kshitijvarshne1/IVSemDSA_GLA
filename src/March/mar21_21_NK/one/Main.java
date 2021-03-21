28f93da1bbdc83238ee3a837315ce742aeb3fd6b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-21 22:47:42 +0530

    final commit

--- src/March/mar21_21_NK/one/Main.java
+++ src/March/mar21_21_NK/one/Main.java
@@ -8,5 +8,13 @@
 package March.mar21_21_NK.one;
 
 public class Main {
+    public static void main(String[] args) {
+        MobilePhone mobilePhone = new MobilePhone(1.0f);
+        try {
+            mobilePhone.sendTextMessage(991888, "hello");
+        } catch (LowBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
