96410ad44f0d6a569e8f600c2dd2f7d48d9c47f5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-22 11:59:41 +0530

    create inOrder Method and implement it

--- src/March/mar22_21_NK/Main.java
+++ src/March/mar22_21_NK/Main.java
@@ -8,5 +8,23 @@
 package March.mar22_21_NK;
 
 public class Main {
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.root = new Node(1);
+        bt.root.left = new Node(4);
+        bt.root.right = new Node(5);
+        bt.root.left = new Node(2);
+        inOrder(bt.root);
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data);
+        inOrder(root.right);
+    }
+
 }
 
