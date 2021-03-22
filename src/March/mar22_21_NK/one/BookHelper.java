7eacf819d5484f16ed62550efdc28f41450c4786
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-22 18:48:46 +0530

    add findAllBookOnSubject method

--- src/March/mar22_21_NK/one/BookHelper.java
+++ src/March/mar22_21_NK/one/BookHelper.java
@@ -7,6 +7,20 @@
 
 package March.mar22_21_NK.one;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class BookHelper {
+    public List<Book> findAllBookOnSubject(Set<Book> books, String requiredSubject) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getSubject().equalsIgnoreCase(requiredSubject)) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+
 }
 
