61ba02ccf9426c3c859806f55df11f59f9ef63d6
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-22 11:31:47 +0530

    create fields and con structor

--- src/March/mar22_21_NK/Node.java
+++ src/March/mar22_21_NK/Node.java
@@ -8,5 +8,15 @@
 package March.mar22_21_NK;
 
 public class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
 }
 
