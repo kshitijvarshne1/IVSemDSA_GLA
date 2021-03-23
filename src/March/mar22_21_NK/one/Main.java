8d37f3dd01b4142276f1a158cd8d7490a8d727e8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-23 20:53:31 +0530

    final commit

--- src/March/mar22_21_NK/one/Main.java
+++ src/March/mar22_21_NK/one/Main.java
@@ -7,20 +7,30 @@
 
 package March.mar22_21_NK.one;
 
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Book> books = new ArrayList<>();
+        Set<Book> books = new HashSet<>();
         books.add(new Book(2234, "amandeep", "Amandeep", "Java"));
         books.add(new Book(3234, "Rahul", "Amandeep", "Python"));
         books.add(new Book(4234, "amandeep", "Amandeep", "C"));
         books.add(new Book(34, "amandeep", "Amandeepkabeta", "C++"));
         books.add(new Book(1734, "aman", "Amandeep", "DSA"));
+        BookHelper bh = new BookHelper();
+        System.out.println(bh.findAllBookOnSubject(books, "Java"));
         System.out.println(books);
-        books.sort(Book::compareTo);
-        System.out.println(books);
+        /*List<Book> sortedList = new ArrayList<>(books);
+        sortedList.sort(Book::compareTo);*/
+        /*System.out.println(sortedList);*/
+
+        List<?> a = books.stream().sorted(Comparator.comparing(Book::getSubject)).collect(Collectors.toList());
+        ;//comparator - how you want to sort it.collect(Collectors.toList());
+        System.out.println(a);
     }
 }
 
