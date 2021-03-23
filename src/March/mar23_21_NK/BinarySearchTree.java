3158d355b1f9236e33c3c04d6acd1012f105805f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-24 00:26:12 +0530

    add insert and preorder method

--- src/March/mar23_21_NK/BinarySearchTree.java
+++ src/March/mar23_21_NK/BinarySearchTree.java
@@ -14,5 +14,31 @@ public class BinarySearchTree {
         this.root = root;
     }
 
+    public void insert(Node newNode) {
+        insert(this.root, newNode);
+    }
+
+    private void insert(Node node, Node newNode) {
+        if (node == null) {
+            node = newNode;
+        }
+        if (newNode.data <= node.data) {
+            insert(node.left, newNode);
+        }
+        insert(node.right, newNode);
+    }
+
+    public void preorder() {
+        preorder(this.root);
+    }
+
+    private void preorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.data + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
 }
 
