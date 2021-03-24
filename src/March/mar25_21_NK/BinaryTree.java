ff4607afec81bf396c98d1d27cbbfdac49e2cbbc
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-25 00:35:31 +0530

    create BinaryTree.java

---
+++ src/March/mar25_21_NK/BinaryTree.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 25-Mar-21
+ *   Time: 12:35 AM
+ *   File: BinaryTree.java
+ */
+
+package March.mar25_21_NK;
+
+public class BinaryTree {
+}
+
