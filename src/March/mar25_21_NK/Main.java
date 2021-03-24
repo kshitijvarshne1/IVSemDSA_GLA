bf10b5d0b3ff069b5b8e2db6e8e4a261e64f56a8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-25 00:41:56 +0530

    implements the methods of traversing

--- src/March/mar25_21_NK/Main.java
+++ src/March/mar25_21_NK/Main.java
@@ -8,5 +8,17 @@
 package March.mar25_21_NK;
 
 public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(new Node(5));
+        bst.insert(new Node(2));
+        bst.insert(new Node(8));
+        bst.preorder();
+        System.out.println();
+        bst.inorder();
+        System.out.println();
+        bst.postorder();
+    }
+
 }
 
