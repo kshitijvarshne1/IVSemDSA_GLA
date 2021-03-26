81d4e1a1d4e65237eb6d52ca709249f44c747e8c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-26 20:04:17 +0530

    implements the insert method

--- src/March/mar26_21_NK/Main.java
+++ src/March/mar26_21_NK/Main.java
@@ -10,11 +10,11 @@ package March.mar26_21_NK;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree bt = new BinarySearchTree();
-        bt.insert(new Node(10));
+        bt.insertRecursive(new Node(10));
         bt.insert(new Node(8));
-        bt.insert(new Node(12));
-        bt.insert(new Node(6));
-        bt.insert(new Node(9));
+        bt.insertRecursive(new Node(12));
+        bt.insertRecursive(new Node(6));
+        bt.insertRecursive(new Node(9));
         bt.preorder();
         System.out.println();
         bt.inorder();
