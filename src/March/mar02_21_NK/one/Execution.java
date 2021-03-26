8e84425434cb85b733f24cd045278b60edb6ded2
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-26 19:24:52 +0530

    final commit

--- src/March/mar02_21_NK/one/Execution.java
+++ src/March/mar02_21_NK/one/Execution.java
@@ -35,6 +35,7 @@ public class Execution {
         ll.print();
         ll.addNodeOnKPosition(7, new Node(100011));
         ll.print();
+
     }
 
     private static int countNumberOfNodes(Node node) {
