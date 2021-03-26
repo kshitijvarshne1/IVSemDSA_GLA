afeedff0b8ddba38c5c13557f603b3cbaeefb4e0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-26 19:53:56 +0530

    implements the methods of traversal technique

--- src/March/mar26_21_NK/Main.java
+++ src/March/mar26_21_NK/Main.java
@@ -8,5 +8,19 @@
 package March.mar26_21_NK;
 
 public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree bt = new BinarySearchTree();
+        bt.insert(new Node(10));
+        bt.insert(new Node(8));
+        bt.insert(new Node(12));
+        bt.insert(new Node(6));
+        bt.insert(new Node(9));
+        bt.preorder();
+        System.out.println();
+        bt.inorder();
+        System.out.println();
+        bt.postorder();
+    }
+
 }
 
