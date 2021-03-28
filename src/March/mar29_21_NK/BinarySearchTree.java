28c0aa98bf63f344ae87a5d7a02c30a037753981
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-29 00:42:31 +0530

    add fields , constructor , insert and inorder method

--- src/March/mar29_21_NK/BinarySearchTree.java
+++ src/March/mar29_21_NK/BinarySearchTree.java
@@ -8,5 +8,41 @@
 package March.mar29_21_NK;
 
 public class BinarySearchTree {
+    Node root;
+
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    public void insert(Node newNode) {
+        root = insertANode(this.root, newNode);
+    }
+
+    private Node insertANode(Node root, Node newNode) {
+        if (root == null) {
+            root = newNode;
+            return root;
+        }
+        if (newNode.data <= root.data) {
+            root.left = insertANode(root.left, newNode);
+        } else {
+            root.right = insertANode(root.right, newNode);
+        }
+        return root;
+    }
+
+    public void inorder() {
+        inorderPrinting(this.root);
+    }
+
+    private void inorderPrinting(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorderPrinting(root.left);
+        System.out.print(root.data + " ");
+        inorderPrinting(root.right);
+    }
+
 }
 
