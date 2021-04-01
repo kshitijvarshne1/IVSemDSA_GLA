a8bc22ae8e211256fc1f70d7e5637b667a1941e3
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-01 11:18:12 +0530

    add fields , constructor and toString method

--- src/April/api01_21_NK/Node.java
+++ src/April/api01_21_NK/Node.java
@@ -8,5 +8,24 @@
 package April.api01_21_NK;
 
 public class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
 }
 
