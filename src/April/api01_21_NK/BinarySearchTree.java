efb624760f626dcc2222134a1d1671c0b21ea9f5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-01 11:18:50 +0530

    create BinarySearchTree.java

---
+++ src/April/api01_21_NK/BinarySearchTree.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 01-Apr-21
+ *   Time: 11:18 AM
+ *   File: BinarySearchTree.java
+ */
+
+package April.api01_21_NK;
+
+public class BinarySearchTree {
+}
+
