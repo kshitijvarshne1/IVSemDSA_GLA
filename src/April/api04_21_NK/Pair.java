4faf50ec74d76745ae5ebc4aa2fad371f235da57
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-04 15:46:40 +0530

    create Pair.java

---
+++ src/April/api04_21_NK/Pair.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 04-Apr-21
+ *   Time: 3:46 PM
+ *   File: Pair.java
+ */
+
+package April.api04_21_NK;
+
+public class Pair {
+}
+
