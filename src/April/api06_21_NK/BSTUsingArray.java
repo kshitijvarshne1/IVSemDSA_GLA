076fcbb0231d06fc2b70a14e7a803be4aed05931
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-06 23:31:14 +0530

    add field and constructor

--- src/April/api06_21_NK/BSTUsingArray.java
+++ src/April/api06_21_NK/BSTUsingArray.java
@@ -8,5 +8,13 @@
 package April.api06_21_NK;
 
 public class BSTUsingArray {
+    public int[] arr;
+
+    public BSTUsingArray(int size) {
+        arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = -1;
+        }
+    }
 }
 
