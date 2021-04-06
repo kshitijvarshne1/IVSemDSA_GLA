71f42b1678738de8ef199fa1469b5bc3b9a92534
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-06 23:37:55 +0530

    add insert method

--- src/April/api06_21_NK/BSTUsingArray.java
+++ src/April/api06_21_NK/BSTUsingArray.java
@@ -8,12 +8,40 @@
 package April.api06_21_NK;
 
 public class BSTUsingArray {
-    public int[] arr;
+    public Integer[] arr;
 
     public BSTUsingArray(int size) {
-        arr = new int[size];
+        arr = new Integer[size];
         for (int i = 0; i < size; i++) {
-            arr[i] = -1;
+            arr[i] = null;
+        }
+    }
+
+    public void insert(int data) {
+        if (arr[0] == null) {
+            arr[0] = data;
+        } else {
+            insert(0, data);
+        }
+    }
+
+    private void insert(int root, int data) {
+        if (arr[root] == null) {
+            return;
+        }
+        if (data <= arr[root]) {
+            if (arr[root * 2 + 1] == null) {
+                arr[root * 2 + 1] = data;
+                return;
+            } else {
+                insert(root * 2 + 1, data);
+            }
+        } else {
+            if (arr[root * 2 + 2] == null) {
+                arr[root * 2 + 2] = data;
+            } else {
+                insert(root * 2 + 2, data);
+            }
         }
     }
 }
