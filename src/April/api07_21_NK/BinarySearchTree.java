fe3caf2ac923b91939ece3bbf8bbb3c188f61706
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-07 22:47:16 +0530

    add insert method

--- src/April/api07_21_NK/BinarySearchTree.java
+++ src/April/api07_21_NK/BinarySearchTree.java
@@ -14,5 +14,29 @@ public class BinarySearchTree {
         this.root = null;
     }
 
+    public void insert(Node newNode) {
+        if (root == null) {
+            root = newNode;
+        } else {
+            Node current = this.root;
+            Node parent;
+            while (true) {
+                parent = current;
+                if (newNode.data <= current.data) {
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = newNode;
+                        return;
+                    }
+                } else {
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
 
