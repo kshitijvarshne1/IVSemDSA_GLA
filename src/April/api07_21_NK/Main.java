4a1765b21f7e731066fbbdc1c5c4d8b7ee021419
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-07 22:51:07 +0530

    implement the method insert and inorder

--- src/April/api07_21_NK/Main.java
+++ src/April/api07_21_NK/Main.java
@@ -8,5 +8,16 @@
 package April.api07_21_NK;
 
 public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree btree = new BinarySearchTree();
+        btree.insert(new Node(100));
+        btree.insert(new Node(92));
+        btree.insert(new Node(109));
+        btree.insert(new Node(88));
+        btree.insert(new Node(96));
+        btree.insert(new Node(105));
+        btree.insert(new Node(111));
+        btree.inorder();
+    }
 }
 
