b556eeadd776b7a2341c20020dbe8b20db3a2f97
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-07 22:48:51 +0530

    create Main.java

---
+++ src/April/api07_21_NK/Main.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 07-Apr-21
+ *   Time: 10:48 PM
+ *   File: Main.java
+ */
+
+package April.api07_21_NK;
+
+public class Main {
+}
+
