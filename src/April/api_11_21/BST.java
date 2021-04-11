b5430a9185eb206e1ca906ca48da1186c06539f3
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-11 12:00:46 +0530

    create BST.java

---
+++ src/April/api_11_21/BST.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Apr-21
+ *   Time: 12:00 PM
+ *   File: BST.java
+ */
+
+package April.api_11_21;
+
+public class BST {
+}
+
