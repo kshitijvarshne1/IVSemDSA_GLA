a2b3aa5a6f6a5bc42de00b9318618f51536540e8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-11 12:16:43 +0530

    implements the method insert, inorder and bfs method

--- src/April/api_11_21/Main.java
+++ src/April/api_11_21/Main.java
@@ -8,5 +8,16 @@
 package April.api_11_21;
 
 public class Main {
+    public static void main(String[] args) {
+        BST bstree = new BST();
+        bstree.insert(new Node(100));
+        bstree.inorder();
+        bstree.insert(new Node(95));
+        bstree.inorder();
+        bstree.insert(new Node(110));
+        bstree.inorder();
+        bstree.bfs();
+    }
+
 }
 
