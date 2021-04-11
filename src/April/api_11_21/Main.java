be0e0438fa55a132c84a02cca343f0670099a566
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-11 12:20:00 +0530

    final commit

--- src/April/api_11_21/Main.java
+++ src/April/api_11_21/Main.java
@@ -11,10 +11,16 @@ public class Main {
     public static void main(String[] args) {
         BST bstree = new BST();
         bstree.insert(new Node(100));
-        bstree.inorder();
         bstree.insert(new Node(95));
-        bstree.inorder();
         bstree.insert(new Node(110));
+        bstree.insert(new Node(90));
+        bstree.insert(new Node(97));
+        bstree.insert(new Node(101));
+        bstree.insert(new Node(111));
+        bstree.insert(new Node(85));
+        bstree.insert(new Node(92));
+        bstree.insert(new Node(96));
+        bstree.insert(new Node(98));
         bstree.inorder();
         bstree.bfs();
     }
