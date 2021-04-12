f753a672915d0cc6e9a428b7a6895982808f4962
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-12 21:50:09 +0530

    add instance variable and constructor

--- src/April/api_12_21_NK/Node.java
+++ src/April/api_12_21_NK/Node.java
@@ -8,5 +8,17 @@
 package April.api_12_21_NK;
 
 public class Node {
+    public int data;
+    public Node left;
+    public Node right;
+    public int height;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.height = 0;
+    }
+
 }
 
