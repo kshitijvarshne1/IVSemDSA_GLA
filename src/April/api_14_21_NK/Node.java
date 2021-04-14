1725b3d4226b068c93d57ebee21b2a116228c4f7
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-14 19:58:51 +0530

    add getter and setter method

--- src/April/api_14_21_NK/Node.java
+++ src/April/api_14_21_NK/Node.java
@@ -17,5 +17,29 @@ public class Node<E> {
         this.left = null;
         this.right = null;
     }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+
+    public Node<E> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
 }
 
