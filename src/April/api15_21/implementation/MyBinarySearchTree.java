9b400129d06bc0f8195d9946a9fb361a2633d8fc
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-15 22:18:29 +0530

    add insert method

--- src/April/api15_21/implementation/MyBinarySearchTree.java
+++ src/April/api15_21/implementation/MyBinarySearchTree.java
@@ -7,53 +7,74 @@
 
 package April.api15_21.implementation;
 
-import April.api15_21.myinterface.BinarySearchTreeADT;
-import April.api15_21.myinterface.Node;
 
-public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT {
+import April.api15_21.myinterface.BinarySearchTreeADT;
+
+public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     public Node<E> root;
 
     public MyBinarySearchTree() {
         this.root = null;
     }
 
-    @Override
-    public void insert(Comparable data) {
 
+    @Override
+    public void insert(E data) {
+        if (root == null) {
+            root = new Node<>(data);
+        } else {
+            Node<E> parent;
+            Node<E> current = root;
+            while (current != null) {
+                parent = current;
+                if (data.compareTo(current.getData()) <= 0) {
+                    current = current.getLeft();
+                    if (current == null) {
+                        parent.setLeft(new Node<>(data));
+                    }
+                } else {
+                    current = current.getRight();
+                    if (current == null) {
+                        parent.setRight(new Node<>(data));
+                    }
+                }
+            }
+
+        }
     }
 
     @Override
-    public boolean search(Comparable searchElement) {
+    public boolean search(E searchElement) {
         return false;
     }
 
     @Override
-    public void inOrder(Node node) {
+    public void inOrder(April.api15_21.myinterface.Node<E> node) {
 
     }
 
     @Override
-    public void preOrder(Node node) {
+    public void preOrder(April.api15_21.myinterface.Node<E> node) {
 
     }
 
     @Override
-    public void postOrder(Node node) {
+    public void postOrder(April.api15_21.myinterface.Node<E> node) {
 
     }
 
     @Override
-    public void reverseInOrder(Node node) {
+    public void reverseInOrder(April.api15_21.myinterface.Node<E> node) {
 
     }
 
     @Override
-    public void delete(Comparable data) {
+    public void delete(E data) {
 
     }
 
     @Override
-    public int height(Node node) {
+    public int height(April.api15_21.myinterface.Node<E> node) {
         return 0;
     }
 }
