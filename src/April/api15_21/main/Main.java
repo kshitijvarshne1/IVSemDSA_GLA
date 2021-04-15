785d23f526046395dc2e577c7be763d1a9c4c30e
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-15 22:58:42 +0530

    implement the insert , inOrder, preOder, PostOrder method

--- src/April/api15_21/main/Main.java
+++ src/April/api15_21/main/Main.java
@@ -7,6 +7,25 @@
 
 package April.api15_21.main;
 
+import April.api15_21.implementation.MyBinarySearchTree;
+
 public class Main {
+    public static void main(String[] args) {
+        MyBinarySearchTree<Integer> bstree = new MyBinarySearchTree<>();
+        bstree.insert(100);
+        bstree.insert(90);
+        bstree.insert(110);
+        bstree.insert(85);
+        bstree.insert(95);
+        bstree.insert(105);
+        bstree.insert(115);
+        bstree.inOrder(bstree.root);
+        System.out.println();
+        bstree.preOrder(bstree.root);
+        System.out.println();
+        bstree.postOrder(bstree.root);
+        System.out.println();
+        System.out.println(bstree.height(bstree.root));
+    }
 }
 
