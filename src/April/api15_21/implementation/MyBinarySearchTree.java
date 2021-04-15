0145c6146c0dac01887b24e531da873f4020cd84
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-15 22:01:31 +0530

    add fields and constructor

--- src/April/api15_21/implementation/MyBinarySearchTree.java
+++ src/April/api15_21/implementation/MyBinarySearchTree.java
@@ -7,6 +7,54 @@
 
 package April.api15_21.implementation;
 
-public class MyBinarySearchTree {
+import April.api15_21.myinterface.BinarySearchTreeADT;
+import April.api15_21.myinterface.Node;
+
+public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT {
+    public Node<E> root;
+
+    public MyBinarySearchTree() {
+        this.root = null;
+    }
+
+    @Override
+    public void insert(Comparable data) {
+
+    }
+
+    @Override
+    public boolean search(Comparable searchElement) {
+        return false;
+    }
+
+    @Override
+    public void inOrder(Node node) {
+
+    }
+
+    @Override
+    public void preOrder(Node node) {
+
+    }
+
+    @Override
+    public void postOrder(Node node) {
+
+    }
+
+    @Override
+    public void reverseInOrder(Node node) {
+
+    }
+
+    @Override
+    public void delete(Comparable data) {
+
+    }
+
+    @Override
+    public int height(Node node) {
+        return 0;
+    }
 }
 
