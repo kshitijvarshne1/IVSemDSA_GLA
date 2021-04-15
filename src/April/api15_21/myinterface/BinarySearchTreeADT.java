8f6b69bfe79a3c9b07022275294e402091d22282
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-15 21:53:25 +0530

    add abstract method

--- src/April/api15_21/myinterface/BinarySearchTreeADT.java
+++ src/April/api15_21/myinterface/BinarySearchTreeADT.java
@@ -1,4 +1,20 @@
 package April.api15_21.myinterface;
 
-public interface BinarySearchTreeADT {
+public interface BinarySearchTreeADT<E extends Comparable<E>> {
+    void insert(E data);
+
+    boolean search(E searchElement);
+
+    void inOrder(Node<E> node);
+
+    void preOrder(Node<E> node);
+
+    void postOrder(Node<E> node);
+
+    void reverseInOrder(Node<E> node);
+
+    void delete(E data);
+
+    int height(Node<E> node);
+
 }
