7ef0265010ff306aed0c3d782ed8ea6c08ff5dc5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-16 17:26:48 +0530

    solve LL add rightRightRotation method

--- src/April/api_12_21_NK/AVL.java
+++ src/April/api_12_21_NK/AVL.java
@@ -21,5 +21,38 @@ public class AVL {
     public int height(Node node) {
         return node == null ? -1 : node.height;
     }
+
+    public void insert(Node newNode) {
+        this.root = insert(this.root, newNode);
+    }
+
+    private Node insert(Node root, Node newNode) {
+        if (root == null) {
+            root = newNode;
+            return root;
+        } else if (newNode.data < root.data) {
+            root.left = insert(root.left, newNode);
+            if (height(root.left) - height(root.right) == 2) {
+                if (newNode.data < root.left.data) {
+                    // left skewed tree
+                    // Problem :- LL
+                    root = rightRightRotation(root);
+                }
+            }
+        } else if (newNode.data > root.data) {
+            root.right = insert(root.right, newNode);
+        }
+        root.height = max(height(root.left), height(root.right)) + 1;
+        return root;
+    }
+
+    private Node rightRightRotation(Node root) {
+        Node newNode = root.left;
+        if (root.left.right != null) {
+            root.left = root.left.right;
+        }
+        newNode.right = root;
+        return newNode;
+    }
 }
 
