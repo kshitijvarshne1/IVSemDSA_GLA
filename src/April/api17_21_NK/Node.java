d580b62a895e600790024157216db8b3db72820c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-17 12:51:46 +0530

    create Node.java

---
+++ src/April/api17_21_NK/Node.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 17-Apr-21
+ *   Time: 12:51 PM
+ *   File: Node.java
+ */
+
+package April.api17_21_NK;
+
+public class Node {
+}
+
