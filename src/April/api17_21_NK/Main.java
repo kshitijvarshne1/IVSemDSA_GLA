e881f8000ddf1b405fc9262d332e7cf127ae9d88
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-17 14:11:46 +0530

    implement the insert and bfs method

--- src/April/api17_21_NK/Main.java
+++ src/April/api17_21_NK/Main.java
@@ -7,6 +7,16 @@
 
 package April.api17_21_NK;
 
+
 public class Main {
+    public static void main(String[] args) {
+        AVL tree = new AVL();
+        tree.insert(new Node(100));
+        tree.bfs();
+        tree.insert(new Node(95));
+        tree.bfs();
+        tree.insert(new Node(91));
+        tree.bfs();
+    }
 }
 
