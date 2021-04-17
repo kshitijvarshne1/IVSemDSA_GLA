6b2a2f7a88a7cc4f015d09141593db0338f669a0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-17 12:53:25 +0530

    add fields and constructor

--- src/April/api17_21_NK/Node.java
+++ src/April/api17_21_NK/Node.java
@@ -8,5 +8,19 @@
 package April.api17_21_NK;
 
 public class Node {
+    // instance variable   :---: Fields
+    public int data;
+    public Node left;
+    public Node right;
+    public int height;
+
+    //Constructor
+    public Node(int data, int height) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.height = height;
+    }
+
 }
 
