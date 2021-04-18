5ca24a58826044a35103c8d08121e02dc033e94c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 18:11:01 +0530

    create BinarySearch.java

---
+++ src/April/api18_21_NK/BinarySearch.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 18-Apr-21
+ *   Time: 6:10 PM
+ *   File: BinarySearch.java
+ */
+
+package April.api18_21_NK;
+
+public class BinarySearch {
+}
+
