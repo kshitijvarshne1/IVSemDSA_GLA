06c4d0ddda588190d1eb042a735d843734e909c4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 17:55:17 +0530

    create LinearSearch.java

---
+++ src/April/api18_21_NK/LinearSearch.java
@@ -0,0 +1,13 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 18-Apr-21
+ *   Time: 5:44 PM
+ *   File: LinearSearch.java
+ */
+
+package April.api18_21_NK;
+
+public class LinearSearch {
+
+}
+
