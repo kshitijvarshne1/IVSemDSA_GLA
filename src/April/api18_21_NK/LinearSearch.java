084d2431a2c3cfa9042b144377c74f5e0b0765df
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 18:02:27 +0530

    implement the LinearSearch algorithm

--- src/April/api18_21_NK/LinearSearch.java
+++ src/April/api18_21_NK/LinearSearch.java
@@ -7,7 +7,30 @@
 
 package April.api18_21_NK;
 
-public class LinearSearch {
+import java.util.Scanner;
 
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements :-");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Enter the search element");
+        int element = sc.nextInt();
+        System.out.println(searchElement(a, element));
+    }
+
+    private static int searchElement(int[] a, int element) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
