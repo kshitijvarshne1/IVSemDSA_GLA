7c11d2f45cd392d2480cdc0430f1632315756c68
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-19 14:35:23 +0530

    implement the insert and bfs method

--- src/April/api19_21_NK/Main.java
+++ src/April/api19_21_NK/Main.java
@@ -8,5 +8,15 @@
 package April.api19_21_NK;
 
 public class Main {
+    public static void main(String[] args) {
+        BST bstree = new BST();
+        bstree.insert(new Node(100));
+        bstree.bfs();
+        bstree.insert(new Node(90));
+        bstree.bfs();
+        bstree.insert(new Node(110));
+        bstree.bfs();
+
+    }
 }
 
