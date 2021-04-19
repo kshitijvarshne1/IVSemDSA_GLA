30d99feb355d478330fb550c72e92c65c473cd9a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-19 15:09:20 +0530

    add delete method

--- src/April/api19_21_NK/BST.java
+++ src/April/api19_21_NK/BST.java
@@ -50,10 +50,10 @@ public class BST {
                 if (temp != null) {
                     System.out.print(temp.data + " ");
                     if (temp.left != null) {
-                        temp = temp.left;
+                        q.add(temp.left);
                     }
                     if (temp.right != null) {
-                        temp = temp.right;
+                        q.add(temp.right);
                     }
                 } else if (!q.isEmpty()) {
                     System.out.println();
@@ -63,5 +63,88 @@ public class BST {
             System.out.println();
         }
     }
+
+    public void delete(int deleteElement) {
+        if (root != null) {
+            Node current = this.root;
+            Node parent = null;
+            while (current != null) {
+                if (current.data == deleteElement) {
+                    break;
+                }
+                parent = current;
+                if (deleteElement < current.data) {
+                    current = current.left;
+                } else {
+                    current = current.right;
+                }
+            }
+            if (current != null) {
+                //Leaf node
+                if (current.left == null && current.right == null) {
+                    // delete element is root
+                    if (parent == null) {
+                        root = null;
+                    } else {
+                        if (deleteElement < parent.data) {
+                            parent.left = null;
+                        } else {
+                            parent.right = null;
+                        }
+                    }
+                }
+                // only left child
+                else if (current.left != null && current.right == null) {
+                    if (parent == null) {
+                        root = root.left;
+                    } else {
+                        if (deleteElement < parent.data) {
+                            parent.left = current.left;
+                        } else {
+                            parent.right = current.left;
+                        }
+                    }
+                } else if (current.left == null && current.right != null) {
+                    if (parent == null) {
+                        root = root.right;
+                    } else {
+                        if (deleteElement < parent.data) {
+                            parent.left = current.right;
+                        } else {
+                            parent.right = current.right;
+                        }
+                    }
+                }
+                // Node having two child
+                else {
+                    Node successorElement = successor(current);
+                    delete(successorElement.data);
+                    successorElement.left = current.left;
+                    successorElement.right = current.right;
+                    // root case
+                    if (parent == null) {
+                        root = successorElement;
+                    } else {
+                        if (deleteElement < parent.data) {
+                            parent.left = successorElement;
+                        } else {
+                            parent.right = successorElement;
+                        }
+                    }
+                }
+            } else {
+                System.out.println("Element not found");
+            }
+        }
+    }
+
+    private Node successor(Node current) {
+        Node temp = current.right;
+        while (temp.left != null) {
+            temp = temp.left;
+        }
+        return temp;
+    }
+
 }
 
