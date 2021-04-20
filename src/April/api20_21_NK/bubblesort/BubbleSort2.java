aaca2b5bd373e422fbca81ca22d902e1c2e04cc4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-20 18:24:57 +0530

    final commit

--- src/April/api20_21_NK/bubblesort/BubbleSort2.java
+++ src/April/api20_21_NK/bubblesort/BubbleSort2.java
@@ -13,7 +13,7 @@ public class BubbleSort2 {
     public static void main(String[] args) {
         int[] arr = {5, 1, 4, 2, 8};
         int p = 1;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
@@ -29,26 +29,3 @@ public class BubbleSort2 {
         System.out.println(Arrays.toString(arr));
     }
 }
-    /*First Pass:
-        ( 5 1 4 2 8 ) -> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
-        ( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4
-        ( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2
-        ( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
-
-        Second Pass:
-        ( 1 4 2 5 8 ) –> ( 1 4 2 5 8 )
-        ( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2
-        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
-        ( 1 2 4 5 8 ) –>  ( 1 2 4 5 8 )
-        Now, the array is already sorted, but our algorithm does not know if it is completed. The algorithm needs one whole pass without any swap to know it is sorted.
-
-        Third Pass:
-        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
-        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
-        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
-        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
-*/
-// Forth pass array is already sorted
-//     ( 1 2 4 5 8 )
-// Fifth path array is already sorted
-//     ( 1 2 4 5 8 )
