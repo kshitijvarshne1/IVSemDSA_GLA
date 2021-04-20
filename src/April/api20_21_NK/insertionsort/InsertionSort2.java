2effab9e06dbd18e815a8958d13628ad12797fb5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-20 19:13:00 +0530

    create InsertionSort2.java

---
+++ src/April/api20_21_NK/insertionsort/InsertionSort2.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 20-Apr-21
+ *   Time: 7:12 PM
+ *   File: InsertionSort2.java
+ */
+
+package April.api20_21_NK.insertionsort;
+
+public class InsertionSort2 {
+}
+
