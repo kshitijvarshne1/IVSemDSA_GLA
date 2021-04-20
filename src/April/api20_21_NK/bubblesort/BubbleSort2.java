ae812df7c1a70dbdeaba6476773c100c3b139fa5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-20 18:17:14 +0530

    add code for bubble sort

--- src/April/api20_21_NK/bubblesort/BubbleSort2.java
+++ src/April/api20_21_NK/bubblesort/BubbleSort2.java
@@ -7,6 +7,24 @@
 
 package April.api20_21_NK.bubblesort;
 
+import java.util.Arrays;
+
 public class BubbleSort2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 9, 4, 6, 3};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+    }
 }
 
