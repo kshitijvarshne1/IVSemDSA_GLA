a34602785c04a9cc6386662ae00acf9b38e8a593
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-20 18:51:02 +0530

    add description for selection sort

--- src/April/api20_21_NK/selectionsort/SelectionSortAscending.java
+++ src/April/api20_21_NK/selectionsort/SelectionSortAscending.java
@@ -6,7 +6,20 @@
  */
 
 package April.api20_21_NK.selectionsort;
+//Selection Sort -> combination of searching and swaping
+
+
+/*
+The algorithm maintains two subarrays in a given array.
+
+1) The subarray which is already sorted. ( initially it is empty )
+2) Remaining subarray which is unsorted.
+
+In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray.*/
 
 public class SelectionSortAscending {
+    public static void main(String[] args) {
+        int[] a = {64, 25, 12, 22, 11};
+    }
 }
 
