e0a6f70e0010a59215b754216bedc2d22a183131
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-20 19:01:11 +0530

    implement the code for selection sort

--- src/April/api20_21_NK/selectionsort/SelectionSortAscending.java
+++ src/April/api20_21_NK/selectionsort/SelectionSortAscending.java
@@ -17,9 +17,24 @@ The algorithm maintains two subarrays in a given array.
 
 In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray.*/
 
+import java.util.Arrays;
+
 public class SelectionSortAscending {
     public static void main(String[] args) {
-        int[] a = {64, 25, 12, 22, 11};
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min_index]) {
+                    min_index = j;
+                }
+            }
+            int temp = arr[min_index];
+            arr[min_index] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
 
