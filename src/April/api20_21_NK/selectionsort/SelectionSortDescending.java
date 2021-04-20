c35a42085e6771ff6cf31c9c7b00e8be942bce40
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-20 19:09:49 +0530

    add code for descending order

--- src/April/api20_21_NK/selectionsort/SelectionSortDescending.java
+++ src/April/api20_21_NK/selectionsort/SelectionSortDescending.java
@@ -7,6 +7,26 @@
 
 package April.api20_21_NK.selectionsort;
 
+import java.util.Arrays;
+
 public class SelectionSortDescending {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int max_value_index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[max_value_index]) {
+                    max_value_index = j;
+                }
+            }
+
+            int temp = arr[max_value_index];
+            arr[max_value_index] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
 
