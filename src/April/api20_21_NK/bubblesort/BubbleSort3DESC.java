c5abd7d9626161195a9b13c4f986fcc8a787af43
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-21 20:54:02 +0530

    add code for descending sorting

--- src/April/api20_21_NK/bubblesort/BubbleSort3DESC.java
+++ src/April/api20_21_NK/bubblesort/BubbleSort3DESC.java
@@ -7,10 +7,34 @@
 
 package April.api20_21_NK.bubblesort;
 
+import java.util.Arrays;
+
 public class BubbleSort3DESC {
     public static void main(String[] args) {
-        int[] a = {5, 1, 4, 2, 8};
-
+        int[] arr = {5, 1, 4, 2, 8};
+        int p = 1;
+        boolean swapped;
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // if there is no swapping means array is already sorted
+            if (swapped == false) {
+                break;
+            }
+            System.out.println("Psss No. " + p);
+            p += 1;
+            System.out.println(Arrays.toString(arr));
+            System.out.println();
+        }
+        System.out.println("==== result ====");
+        System.out.println(Arrays.toString(arr));
     }
 }
 
