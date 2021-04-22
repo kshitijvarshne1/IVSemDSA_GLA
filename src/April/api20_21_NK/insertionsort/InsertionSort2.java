6c0e973f3bac03b1dc19bb81842ccf05b332c0b0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-22 16:38:09 +0530

    add code to implement the sorting in descending

--- src/April/api20_21_NK/insertionsort/InsertionSort2.java
+++ src/April/api20_21_NK/insertionsort/InsertionSort2.java
@@ -8,5 +8,17 @@
 package April.api20_21_NK.insertionsort;
 
 public class InsertionSort2 {
+    public static void main(String[] args) {
+        int[] a = {5, 4, 10, 1, 6, 2};
+        for (int i = 0; i < a.length; i++) {
+            int temp = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j] < temp) {
+                a[j + 1] = a[j];
+                j = j - 1;
+            }
+            a[j + 1] = temp;
+        }
+    }
 }
 
