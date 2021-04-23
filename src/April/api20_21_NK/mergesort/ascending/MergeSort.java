3fe3de0d1214ae695442b206c2aa8ce630d3703b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-23 17:39:29 +0530

    add mergeSort method code

--- src/April/api20_21_NK/mergesort/ascending/MergeSort.java
+++ src/April/api20_21_NK/mergesort/ascending/MergeSort.java
@@ -8,5 +8,36 @@
 package April.api20_21_NK.mergesort.ascending;
 
 public class MergeSort {
+    int[] arr;
+
+    public MergeSort(int size) {
+        arr = new int[size];
+    }
+
+    public void mergeSort(int a[], int l, int r) {
+        if (l < r) {
+            int mid = (l + r) / 2;
+            mergeSort(a, l, mid);
+            mergeSort(a, mid + 1, r);
+            merge(a, l, r, mid);
+        }
+    }
+
+    public void merge(int a[], int l, int r, int mid) {
+        int i = l;
+        int j = mid + 1;
+        int k = l;
+        while (i <= mid && j <= r) {
+            if (a[i] <= a[j]) {
+                arr[k] = arr[i];
+                i += 1;
+            } else {
+                arr[k] = a[j];
+                j += 1;
+            }
+            k += 1;
+        }
+
+    }
 }
 
