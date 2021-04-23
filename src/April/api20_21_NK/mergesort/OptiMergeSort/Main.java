c8a7ecdaf85cca3b9788c79f938a942d1911c75b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-23 18:56:05 +0530

    implement the MergeSort class

--- src/April/api20_21_NK/mergesort/OptiMergeSort/Main.java
+++ src/April/api20_21_NK/mergesort/OptiMergeSort/Main.java
@@ -7,6 +7,16 @@
 
 package April.api20_21_NK.mergesort.OptiMergeSort;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 6, 9, 4, 3};
+        MergeSort m = new MergeSort();
+        System.out.println(Arrays.toString(a));
+        m.mergeSort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+    }
+
 }
 
