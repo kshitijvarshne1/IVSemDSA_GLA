bd384b78393c9595c7e1623df4ee919ac7f93b16
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-23 18:52:31 +0530

    final commit

--- src/April/api20_21_NK/mergesort/ascending/Main.java
+++ src/April/api20_21_NK/mergesort/ascending/Main.java
@@ -7,9 +7,14 @@
 
 package April.api20_21_NK.mergesort.ascending;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[] a = {10, 7, 9, 5, 3, 8};
+        MergeSort m = new MergeSort();
+        m.mergeSort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
     }
 }
 
--- src/April/api20_21_NK/mergesort/ascending/MergeSort.java
+++ src/April/api20_21_NK/mergesort/ascending/MergeSort.java
@@ -8,28 +8,61 @@
 package April.api20_21_NK.mergesort.ascending;
 
 public class MergeSort {
-    int[] arr;
-
-    public MergeSort(int size) {
-        arr = new int[size];
-    }
-
-    public void mergeSort(int a[], int l, int r) {
+    public void mergeSort(int[] a, int l, int r) {
         if (l < r) {
             int mid = (l + r) / 2;
             mergeSort(a, l, mid);
             mergeSort(a, mid + 1, r);
-            merge(a, l, r, mid);
+            merge(a, l, mid, r);
         }
     }
 
-    public void merge(int a[], int l, int r, int mid) {
-        int i = l;
+    public void merge(int[] a, int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid; // r-(mid+1)+1
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = a[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = a[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                a[k] = L[i];
+                i++;
+            } else {
+                a[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            a[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            a[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+}
+
+/*
+
+int i = l;
         int j = mid + 1;
         int k = l;
         while (i <= mid && j <= r) {
             if (a[i] <= a[j]) {
-                arr[k] = arr[i];
+                arr[k] = a[i];
                 i += 1;
             } else {
                 arr[k] = a[j];
@@ -51,7 +84,5 @@ public class MergeSort {
                 k += 1;
             }
         }
-    }
 
-}
-
+* */
