d32a952e3d8dcaa2287c5da6889a5d9adbe22f4b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-25 11:41:18 +0530

    add getter and setter method

--- src/April/api25_21_NK/Node.java
+++ src/April/api25_21_NK/Node.java
@@ -17,5 +17,30 @@ public class Node<T extends Comparable<T>> {
         this.left = left;
         this.right = right;
     }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+
 }
 
