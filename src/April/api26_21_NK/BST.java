5c0074262e923e4f2bb2032484fd9dfaa22891db
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-26 19:58:45 +0530

    add inorder method

--- src/April/api26_21_NK/BST.java
+++ src/April/api26_21_NK/BST.java
@@ -29,5 +29,18 @@ public class BST {
         }
         return root;
     }
+
+    public void inorder() {
+        inorder(this.root);
+    }
+
+    private void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+        System.out.println();
+    }
 }
 
