3b802a89f50cdf268a26fcadc6cf5353d801e8ef
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-26 20:10:44 +0530

    implement insert , preorder and inorder

--- src/April/api26_21_NK/Main.java
+++ src/April/api26_21_NK/Main.java
@@ -8,5 +8,15 @@
 package April.api26_21_NK;
 
 public class Main {
+    public static void main(String[] args) {
+        BST bsttree = new BST();
+        bsttree.insert(new Node(100));
+        bsttree.insert(new Node(95));
+        bsttree.insert(new Node(105));
+        bsttree.insert(new Node(85));
+        bsttree.insert(new Node(97));
+        bsttree.inorder();
+        bsttree.preorder();
+    }
 }
 
