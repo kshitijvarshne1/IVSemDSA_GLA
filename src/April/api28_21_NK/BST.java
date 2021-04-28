1ae279abc36f9298bf5bd60d4d113a72f3f0b27c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-28 15:56:33 +0530

    add insert method

--- src/April/api28_21_NK/BST.java
+++ src/April/api28_21_NK/BST.java
@@ -13,5 +13,28 @@ public class BST {
     public BST() {
         this.root = null;
     }
+
+    public void insert(Node newNode) {
+        if (root == null) {
+            root = newNode;
+        } else {
+            Node parent;
+            Node current = this.root;
+            while (current != null) {
+                parent = current;
+                if (current.data <= newNode.data) {
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = newNode;
+                    }
+                } else {
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = newNode;
+                    }
+                }
+            }
+        }
+    }
 }
 
