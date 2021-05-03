0963dd90a1ab2df76e40948b59755c88b815e2a9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-03 18:41:45 +0530

    final commit

--- src/May/one/Graph.java
+++ src/May/one/Graph.java
@@ -34,6 +34,5 @@ public class Graph {
             System.out.println();
         }
     }
-
 }
 
--- src/May/one/Main.java
+++ src/May/one/Main.java
@@ -8,5 +8,13 @@
 package May.one;
 
 public class Main {
+    public static void main(String[] args) {
+        Graph gp = new Graph(4);
+        gp.addEdge(0, 1);
+        gp.addEdge(0, 2);
+        gp.addEdge(1, 2);
+        gp.addEdge(1, 3);
+        gp.printGraph();
+    }
 }
 
