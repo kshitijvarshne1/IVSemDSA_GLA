889bad279e9afea10c545d7944bf79d5ea1f5195
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-05 23:01:16 +0530

    add bfs method

--- src/May/may05_21/MyGraph.java
+++ src/May/may05_21/MyGraph.java
@@ -8,6 +8,8 @@
 package May.may05_21;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MyGraph {
     public ArrayList<ArrayList<Integer>> adj;
@@ -26,13 +28,38 @@ public class MyGraph {
     }
 
     public void printGraph() {
+        int j = 0;
         for (ArrayList<Integer> x : adj) {
-            System.out.print(x + " ->");
+            System.out.print(j + " ->");
             for (Integer i : x) {
                 System.out.print(i + " ");
             }
+            j += 1;
             System.out.println();
         }
+    }
+
+    public void bfs(int source) {
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Boolean> visited = new ArrayList<>();
+        for (int i = 0; i < adj.size(); i++) {
+            visited.add(i, true);
+        }
+        q.add(source);
+        visited.add(source, false);
+        while (!q.isEmpty()) {
+            int temp = q.remove();
+            System.out.print(temp + " ");
+            for (ArrayList<Integer> x : adj) {
+                for (Integer i : x) {
+                    if (visited.get(i) == false) {
+                        visited.add(i, true);
+                        q.add(i);
+                    }
+                }
+            }
+        }
+        System.out.println();
     }
 }
 
