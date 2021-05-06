e9bed7501d8084ad405d9e0aabedd1eabe1981eb
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-06 22:51:17 +0530

    add ShortestPathInUnWeightGraph method

--- src/May/may06_21/Graph.java
+++ src/May/may06_21/Graph.java
@@ -8,6 +8,9 @@
 package May.may06_21;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
     ArrayList<ArrayList<Integer>> adj;
@@ -21,7 +24,7 @@ public class Graph {
 
     public void addEdge(int u, int v) {
         adj.get(u).add(v);
-        adj.get(v).get(u);
+        adj.get(v).add(u);
     }
 
     public void printGraph() {
@@ -30,8 +33,31 @@ public class Graph {
             for (Integer integer : adj.get(i)) {
                 System.out.print(integer + " ");
             }
+            System.out.println();
         }
-        System.out.println();
+    }
+
+    public void ShortestPathInUnWeightGraph(int source) {
+        int[] dist = new int[adj.size()];
+        for (int i = 0; i < adj.size(); i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+        boolean[] visited = new boolean[adj.size()];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(source);
+        visited[source] = true;
+        dist[source] = 0;
+        while (!q.isEmpty()) {
+            int temp = q.remove();
+            for (int x : adj.get(temp)) {
+                if (visited[x] == false) {
+                    dist[x] = dist[temp] + 1;
+                    visited[x] = true;
+                    q.add(x);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dist));
     }
 }
 
