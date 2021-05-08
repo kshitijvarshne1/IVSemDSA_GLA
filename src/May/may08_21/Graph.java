c2fccbda3f90e3437521a7bb899db74df5bccd91
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-08 17:06:24 +0530

    add field , constructor , addEdge and print Graph method

--- src/May/may08_21/Graph.java
+++ src/May/may08_21/Graph.java
@@ -7,6 +7,32 @@
 
 package May.may08_21;
 
+import java.util.ArrayList;
+
 public class Graph {
+    ArrayList<ArrayList<Integer>> adj;
+
+    public Graph(int vertices) {
+        adj = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < adj.size(); i++) {
+            System.out.print(i + " -> ");
+            for (Integer integer : adj.get(i)) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
 
