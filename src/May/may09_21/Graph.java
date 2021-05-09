3df09e1a6827b603ae113257ff0005ffe399fff9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-09 13:59:24 +0530

    add dfsRec method

--- src/May/may09_21/Graph.java
+++ src/May/may09_21/Graph.java
@@ -33,5 +33,34 @@ public class Graph {
             System.out.println();
         }
     }
+
+    public boolean dfs() {
+        boolean[] visited = new boolean[adj.size()];
+        boolean[] recStack = new boolean[adj.size()];
+        for (int i = 0; i < adj.size(); i++) {
+            if (visited[i] == false) {
+                if (dfsRec(adj, i, visited, recStack) == true) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    private boolean dfsRec(ArrayList<ArrayList<Integer>> adj, int i, boolean[] visited, boolean[] recStack) {
+        visited[i] = false;
+        recStack[i] = false;
+        for (Integer j : adj.get(i)) {
+            if (visited[j] == false && dfsRec(adj, j, visited, recStack)) {
+                return true;
+            } else if (recStack[j] == true) {
+                return true;
+            }
+
+        }
+        recStack[i] = true;
+        return false;
+    }
 }
 
