3b2ee3fcee4c25d7c548b13765e3dcf789fc2710
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-11 12:13:57 +0530

    add topological sort

--- src/May/may11_21/Graph.java
+++ src/May/may11_21/Graph.java
@@ -8,6 +8,9 @@
 package May.may11_21;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
     ArrayList<ArrayList<Integer>> adj;
@@ -33,5 +36,34 @@ public class Graph {
             System.out.println();
         }
     }
-}
+    // TopoSort for only Acylic graph
+    // Khan algorithm
 
+    public void toposort() {
+        int[] indegree = new int[adj.size()];
+        Arrays.fill(indegree, 0);
+        // counting for indegree
+        for (int i = 0; i < adj.size(); i++) {
+            for (Integer j : adj.get(i)) {
+                indegree[j]++;
+            }
+        }
+        // insert those vertices whose indegree is zero initially
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        while (!q.isEmpty()) {
+            int temp = q.remove();
+            System.out.print(temp);
+            for (Integer i : adj.get(temp)) {
+                indegree[i]--;
+                if (indegree[i] == 0) {
+                    q.add(i);
+                }
+            }
+        }
+    }
+}
