d4413d37ea1b34cb8d5909ef1359add49c099041
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 20:56:05 +0530

    add field and constructor

--- src/May/may13_21/Graph.java
+++ src/May/may13_21/Graph.java
@@ -7,6 +7,29 @@
 
 package May.may13_21;
 
+import java.util.ArrayList;
+
+// Minimum spanning tree
+// Prism algorithm  -> greedy algorithm
+// Minimize the wire length and make sure that all computers are connected to each other may be through intermediate computers
+// Minimum spanning tree define for only undirected graph
+// result -> no cycle
+// Condition for MST
+// N vertices with N-1 edges
+// every vertices should be reachable by every another vertices
 public class Graph {
+    ArrayList<ArrayList<Pair>> adj;
+
+    public Graph(int vertices) {
+        adj = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+}
+
+class Pair {
+    int v;
+    int weight;
 }
 
