b45112e7010650f8ca7ff2182e7df06a44fd08d9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-14 14:44:41 +0530

    add fields and constructor

--- src/May/may14_21/Graph.java
+++ src/May/may14_21/Graph.java
@@ -7,6 +7,31 @@
 
 package May.may14_21;
 
+import java.util.ArrayList;
+
+// Dijkstra algorithm
+// find shortest path from source to all vertices
+// 1- it does not work in negative weight
+// 2- if we add some edge then the output is changed for all vertices
 public class Graph {
+    public ArrayList<ArrayList<Pair>> adj;
+
+    public Graph(int vertices) {
+        adj = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+}
+
+class Pair {
+    int v;
+    int weight;
+
+    public Pair(int v, int weight) {
+        this.v = v;
+        this.weight = weight;
+    }
 }
 
