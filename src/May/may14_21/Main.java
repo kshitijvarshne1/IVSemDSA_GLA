c299d17d881345d41d2ea51396fe2ad39fe9929b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-14 17:01:57 +0530

    implement dijkstra algo

--- src/May/may14_21/Main.java
+++ src/May/may14_21/Main.java
@@ -7,6 +7,32 @@
 
 package May.may14_21;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        Graph gf = new Graph(5);
+
+        gf.addEdge(0, 1, 2);
+        gf.addEdge(1, 0, 2);
+
+        gf.addEdge(1, 2, 4);
+        gf.addEdge(2, 1, 4);
+
+        gf.addEdge(0, 3, 1);
+        gf.addEdge(3, 0, 1);
+
+        gf.addEdge(3, 2, 3);
+        gf.addEdge(2, 3, 3);
+
+        gf.addEdge(1, 4, 5);
+        gf.addEdge(4, 1, 5);
+
+        gf.addEdge(2, 4, 1);
+        gf.addEdge(4, 2, 1);
+        gf.printGraph();
+        System.out.println(Arrays.toString(gf.dijkstra(0)));
+
+    }
 }
 
