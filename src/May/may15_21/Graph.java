3a0af74a96cb8c7d2bf97efbe1436d8993237459
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-15 14:32:03 +0530

    add fields and constructor

--- src/May/may15_21/Graph.java
+++ src/May/may15_21/Graph.java
@@ -6,8 +6,27 @@
  */
 
 package May.may15_21;
+//Kosaraju's algorithm
+// Strongly Connected Components
+/*
+ *   3 steps
+ *   1 -> Sort all the node inorder to finishing time -> Topo sort ->O(N)
+ *   2 -> Transpose the graph                                      -> O(N+E)
+ *   3 -> dfs according to finishing time means topo sort          -> O(N+E)
+ *   T.C-> O(N+E)
+ * */
+
+
+import java.util.ArrayList;
 
 public class Graph {
+    ArrayList<ArrayList<Integer>> adj;
 
+    public Graph(int vertices) {
+        adj = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
 }
 
