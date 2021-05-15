f50c77f690821c95a68998e5bf5a535ace7eecfb
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-15 15:04:42 +0530

    add kosaraju algo

--- src/May/may15_21/Graph.java
+++ src/May/may15_21/Graph.java
@@ -18,6 +18,8 @@ package May.may15_21;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
 
 public class Graph {
     ArrayList<ArrayList<Integer>> adj;
@@ -41,6 +43,60 @@ public class Graph {
             }
             System.out.println();
         }
+    }
+
+    public void kosaraju() {
+        //Step 1 -> sort all the node inorder to finishing time
+        boolean[] visited = new boolean[adj.size()];
+        Arrays.fill(visited, false);
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < adj.size(); i++) {
+            if (visited[i] == false) {
+                dfs(i, visited, s);
+            }
+        }
+        // Step 2 -> transpose the adj
+        ArrayList<ArrayList<Integer>> tranpose = new ArrayList<>();
+        for (int i = 0; i < adj.size(); i++) {
+            tranpose.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < adj.size(); i++) {
+            visited[i] = false;
+            for (Integer j : adj.get(i)) {
+                tranpose.get(j).add(i);
+            }
+        }
+        // Step 3 -> dfs according to finishing time means topo sort
+        while (!s.isEmpty()) {
+            int node = s.pop();
+            if (visited[node] == false) {
+                System.out.print("SCC:- ");
+                revDfs(node, tranpose, visited);
+                System.out.println();
+            }
+        }
+
+    }
+
+    private void revDfs(int node, ArrayList<ArrayList<Integer>> tranpose, boolean[] visited) {
+        visited[node] = true;
+        System.out.print(node + " ");
+        for (Integer j : tranpose.get(node)) {
+            if (visited[j] == false) {
+                revDfs(j, tranpose, visited);
+            }
+        }
+    }
+
+    private void dfs(int i, boolean[] visited, Stack<Integer> s) {
+        visited[i] = true;
+        for (Integer j : adj.get(i)) {
+            if (visited[j] == false) {
+                dfs(j, visited, s);
+            }
+        }
+        s.push(i);
     }
 }
 
