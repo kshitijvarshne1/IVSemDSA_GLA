794ceb96f300f5ef864bece9b59bc7d07205dca3
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-16 14:16:05 +0530

    add kosaraju algo

--- src/May/may16_21/Graph.java
+++ src/May/may16_21/Graph.java
@@ -8,7 +8,10 @@
 package May.may16_21;
 
 import java.util.ArrayList;
-//Dijkstra algorithm
+import java.util.Arrays;
+import java.util.Stack;
+
+//Kosaraju algorithm
 public class Graph {
     ArrayList<ArrayList<Integer>> adj;
 
@@ -32,5 +35,58 @@ public class Graph {
             System.out.println();
         }
     }
+
+    public void kosaraju() {
+        //Step 1 -> sort all the node inorder to finishing time
+        boolean[] visited = new boolean[adj.size()];
+        Arrays.fill(visited, false);
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < adj.size(); i++) {
+            if (visited[i] == false) {
+                dfsRec(i, visited, s);
+            }
+        }
+        // Step 2 -> transpose the adj
+        ArrayList<ArrayList<Integer>> transpose = new ArrayList<>();
+        for (int i = 0; i < adj.size(); i++) {
+            transpose.add(new ArrayList<>());
+        }
+        for (int i = 0; i < adj.size(); i++) {
+            visited[i] = false;
+            for (Integer j : adj.get(i)) {
+                transpose.get(j).add(i);
+            }
+        }
+        // Step 3 -> dfs according to finishing time means topo sort
+        while (!s.isEmpty()) {
+            int node = s.pop();
+            if (visited[node] == false) {
+                revdfs(node, transpose, visited);
+                System.out.println();
+            }
+        }
+    }
+
+    private void revdfs(int node, ArrayList<ArrayList<Integer>> transpose, boolean[] visited) {
+        visited[node] = true;
+        System.out.print(node + " ");
+        for (Integer j : transpose.get(node)) {
+            if (visited[j] == false) {
+                revdfs(j, transpose, visited);
+            }
+        }
+    }
+
+    private void dfsRec(int i, boolean[] visited, Stack<Integer> s) {
+        visited[i] = true;
+        for (Integer j : adj.get(i)) {
+            if (visited[j] == false) {
+                dfsRec(j, visited, s);
+            }
+        }
+        s.push(i);
+    }
+
 }
 
