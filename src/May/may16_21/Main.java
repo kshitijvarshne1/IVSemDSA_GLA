8bf228d30fcf5d0a42180a3b8ba0b77aef77e528
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-16 14:08:55 +0530

    implement the addEdge and printGraph method

--- src/May/may16_21/Main.java
+++ src/May/may16_21/Main.java
@@ -7,6 +7,16 @@
 
 package May.may16_21;
 
+
 public class Main {
+    public static void main(String[] args) {
+        Graph gf = new Graph(5);
+        gf.addEdge(0, 1);
+        gf.addEdge(1, 2);
+        gf.addEdge(2, 0);
+        gf.addEdge(1, 3);
+        gf.addEdge(3, 4);
+        gf.printGraph();
+    }
 }
 
