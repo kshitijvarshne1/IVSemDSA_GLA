a8613ebbb9fc888afdd9c99daa3cfbe844baa181
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-17 18:49:42 +0530

    create Pair class

--- src/May/may17_21/Graph.java
+++ src/May/may17_21/Graph.java
@@ -6,8 +6,23 @@
  */
 
 package May.may17_21;
+
+import java.util.ArrayList;
+
 // Bellman ford Shortest Path algorithm
 // Work in negative wight
 public class Graph {
+    ArrayList<ArrayList<Pair>> adj;
+
+}
+
+class Pair {
+    int v;
+    int weight;
+
+    public Pair(int v, int weight) {
+        this.v = v;
+        this.weight = weight;
+    }
 }
 
