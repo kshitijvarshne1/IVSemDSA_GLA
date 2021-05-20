462ffb174d8a5e29e5ccfd8e51e70b19bdc0d6db
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-20 18:56:10 +0530

    create Main.java

---
+++ src/May/may20_21/linkedlist/Main.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 20-May-21
+ *   Time: 6:55 PM
+ *   File: Main.java
+ */
+
+package May.may20_21.linkedlist;
+
+public class Main {
+}
+
