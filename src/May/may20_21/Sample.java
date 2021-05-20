b2da0641cdf87845074a7de2f2ec0062b70f5065
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-20 18:16:47 +0530

    create Sample.java

---
+++ src/May/may20_21/Sample.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 20-May-21
+ *   Time: 6:16 PM
+ *   File: Sample.java
+ */
+
+package May.may20_21;
+
+public class Sample {
+}
+
