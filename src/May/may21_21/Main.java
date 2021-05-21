65fc9e80523450b25d5fa8941c0f205acbfcc355
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-21 20:34:17 +0530

    implement the search and insertATMiddle method

--- src/May/may21_21/Main.java
+++ src/May/may21_21/Main.java
@@ -19,6 +19,8 @@ public class Main {
         list.print();
         list.insertAtMiddle(55);
         list.print();
+        System.out.println(list.search(44));
+        System.out.println(list.search(11111));
     }
 }
 
