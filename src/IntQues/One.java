9add4ba6287349cdbcd45f66fddb5710d6335721
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-24 14:59:56 +0530

    create One.java

---
+++ src/IntQues/One.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 24-May-21
+ *   Time: 2:59 PM
+ *   File: one.java
+ */
+
+package IntQues;
+
+public class One {
+}
+
