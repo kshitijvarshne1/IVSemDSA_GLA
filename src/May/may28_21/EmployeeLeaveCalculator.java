78b71eb553f3875549a128aac4c9b40ac8ebff3e
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-28 17:21:11 +0530

    create EmployeeLeaveCalculator.java

---
+++ src/May/may28_21/EmployeeLeaveCalculator.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 28-May-21
+ *   Time: 5:21 PM
+ *   File: EmployeeLeaveCalculator.java
+ */
+
+package May.may28_21;
+
+public class EmployeeLeaveCalculator {
+}
+
