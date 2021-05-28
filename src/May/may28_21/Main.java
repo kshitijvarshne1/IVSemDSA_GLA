eb160f613150554d34b1ad08e82e7ca6a9956ab6
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-28 17:41:19 +0530

    final commit

--- src/May/may28_21/Main.java
+++ src/May/may28_21/Main.java
@@ -13,6 +13,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee((short) 1, "John", 600.0, (byte) 33));
+        employeeList.add(new Employee((short) 2, "sam", 60.0, (byte) 1));
+        employeeList.add(new Employee((short) 3, "tan", 900.0, (byte) 93));
+        employeeList.add(new Employee((short) 4, "panm", 660.0, (byte) 9));
+        employeeList.add(new Employee((short) 5, "kiki", 1000.0, (byte) 3));
+
+        EmployeeLeaveCalculator e = new EmployeeLeaveCalculator();
+        System.out.println(e.getDefaultEmployeeIDList(employeeList).toString());
+
+        System.out.println(e.getNumberOfLeaves(employeeList).toString());
 
 
     }
