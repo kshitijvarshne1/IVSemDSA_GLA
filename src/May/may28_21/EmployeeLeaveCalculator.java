57cc9fcb8e24b7a8b527e633f6c1c0097634d1e9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-28 17:35:43 +0530

    final commit

--- src/May/may28_21/EmployeeLeaveCalculator.java
+++ src/May/may28_21/EmployeeLeaveCalculator.java
@@ -7,6 +7,27 @@
 
 package May.may28_21;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeLeaveCalculator {
+    public List<Short> getDefaultEmployeeIDList(List<Employee> employeeList) {
+        List<Short> result = new ArrayList<>();
+        for (Employee e : employeeList) {
+            if (e.getNoOfLeaves() > 25) {
+                result.add(e.getId());
+                e.setSalary(e.getSalary() * .01 + e.getSalary());
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<String> getNumberOfLeaves(List<Employee> employeeList) {
+        ArrayList<String> result = new ArrayList<>();
+        for (Employee e : employeeList) {
+            result.add(e.getName() + "-" + e.getNoOfLeaves());
+        }
+        return result;
+    }
 }
 
