3b8b1716912dcf5dfd24e5b81a7fc94bb71d7fae
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-28 17:15:33 +0530

    create Employee.java

---
+++ src/May/may28_21/Employee.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 28-May-21
+ *   Time: 5:15 PM
+ *   File: Employee.java
+ */
+
+package May.may28_21;
+
+public class Employee {
+}
+
