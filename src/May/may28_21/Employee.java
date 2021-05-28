409353388b1060fb59218d7a963760493421b717
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-28 17:19:54 +0530

    add fields , constructor, getter and setter method

--- src/May/may28_21/Employee.java
+++ src/May/may28_21/Employee.java
@@ -8,5 +8,41 @@
 package May.may28_21;
 
 public class Employee {
+    private final short id;
+    private final String name;
+    private double salary;
+    private byte noOfLeaves;
+
+    public Employee(short id, String name, double salary, byte noOfLeaves) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.noOfLeaves = noOfLeaves;
+    }
+
+    public short getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public byte getNoOfLeaves() {
+        return noOfLeaves;
+    }
+
+    public void setNoOfLeaves(byte noOfLeaves) {
+        this.noOfLeaves = noOfLeaves;
+    }
+
 }
 
