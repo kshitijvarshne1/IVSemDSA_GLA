64e5779484065975f0190c4ea455d122f0b59348
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-02 14:25:29 +0530

    create Graph.java

---
+++ src/June/jun02_21/Graph.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 02-Jun-21
+ *   Time: 2:25 PM
+ *   File: Graph.java
+ */
+
+package June.jun02_21;
+
+public class Graph {
+}
+
