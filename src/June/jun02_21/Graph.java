590abb8d70325f701481dbf14e9fc35232b5953e
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-02 14:36:22 +0530

    add printGraph method

--- src/June/jun02_21/Graph.java
+++ src/June/jun02_21/Graph.java
@@ -20,9 +20,18 @@ public class Graph {
     }
 
     public void addEdge(int u, int v, int weight) {
-        adj.get(u).add(new May.may14_21.Pair(v, weight));
+        adj.get(u).add(new Pair(v, weight));
     }
 
+    public void printGraph() {
+        for (int i = 0; i < adj.size(); i++) {
+            System.out.print(i + " -> ");
+            for (May.may14_21.Pair p : adj.get(i)) {
+                System.out.print("{ " + p.v + " , " + p.weight + " } ");
+            }
+            System.out.println();
+        }
+    }
 
 }
 
