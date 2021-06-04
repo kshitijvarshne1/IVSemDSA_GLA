da651cd989039b1d66eca78049cb94fd1711e98d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-04 20:45:51 +0530

    add bubbleSort method

--- src/June/jun03_21/BubbleSort.java
+++ src/June/jun03_21/BubbleSort.java
@@ -7,6 +7,25 @@
 
 package June.jun03_21;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 9, -1, 4, 6, 7, -100};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int k = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = k;
+                }
+            }
+        }
+    }
 }
 
