799c68a97e36284ad48f6a7355f7eba728a860c3
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-04 21:05:18 +0530

    add selectionSort method

--- src/June/jun03_21/selectionSort.java
+++ src/June/jun03_21/selectionSort.java
@@ -7,6 +7,30 @@
 
 package June.jun03_21;
 
+import java.util.Arrays;
+
 public class selectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int min_idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
 
