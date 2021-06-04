7c1722dedab66d741324e4fa4889354258ca0c9b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-04 20:59:28 +0530

    add insertionSort method

--- src/June/jun03_21/insertionSort.java
+++ src/June/jun03_21/insertionSort.java
@@ -7,6 +7,27 @@
 
 package June.jun03_21;
 
+import java.util.Arrays;
+
 public class insertionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        insertionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] arr) {
+        int i, key, j;
+        for (i = 1; i < arr.length; i++) {
+            key = arr[i];
+            j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
 
