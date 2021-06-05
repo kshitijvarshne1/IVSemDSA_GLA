46b78a4bf951bf7c643e9d15557c260b9f164934
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-05 15:03:48 +0530

    impelement the basic method of stack

--- src/June/jun05_21/Main.java
+++ src/June/jun05_21/Main.java
@@ -8,5 +8,13 @@
 package June.jun05_21;
 
 public class Main {
+    public static void main(String[] args) {
+        AStack stack = new AStack(4);
+        stack.push(2);
+        stack.push(4);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+
+    }
 }
 
