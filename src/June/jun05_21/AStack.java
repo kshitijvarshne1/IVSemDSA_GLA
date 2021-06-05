c2a74b6ffc2494f11e89334702485aae34e3a496
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-05 14:56:42 +0530

    create AStack.java

---
+++ src/June/jun05_21/AStack.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 05-Jun-21
+ *   Time: 2:56 PM
+ *   File: AStack.java
+ */
+
+package June.jun05_21;
+
+public class AStack {
+}
+
