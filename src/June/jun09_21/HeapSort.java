72bc1563c4ff51c25fa42e59dd541643bc85faa3
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-08 15:34:18 +0530

    add left , right , parent and insert method

--- src/June/jun09_21/HeapSort.java
+++ src/June/jun09_21/HeapSort.java
@@ -17,5 +17,32 @@ public class HeapSort {
         this.currSize = 0;
         this.capacity = capacity;
     }
+
+    public void insert(int value) {
+        if (currSize == capacity) return;
+
+        currSize++;
+        arr[currSize - 1] = value;
+
+        for (int i = currSize - 1; i != 0 && arr[parent(i)] > arr[i]; ) {
+
+            int k = arr[i];
+            arr[i] = arr[parent(i)];
+            arr[parent(i)] = k;
+            i = parent(i);
+        }
+    }
+
+    public int left(int i) {
+        return (2 * i + 1);
+    }
+
+    public int right(int i) {
+        return (2 * i + 2);
+    }
+
+    public int parent(int i) {
+        return (i - 1) / 2;
+    }
 }
 
