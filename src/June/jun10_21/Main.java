e9de81a4d004f09df8e9c547f46dd083f818f87d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-09 17:29:02 +0530

    implement the insert and toString method

--- src/June/jun10_21/Main.java
+++ src/June/jun10_21/Main.java
@@ -8,5 +8,20 @@
 package June.jun10_21;
 
 public class Main {
+    public static void main(String[] args) {
+        HeapSort hs = new HeapSort(9);
+        hs.insert(10);
+        hs.insert(20);
+        hs.insert(15);
+        hs.insert(40);
+        hs.insert(50);
+        hs.insert(100);
+        hs.insert(25);
+        hs.insert(45);
+        System.out.println(hs);
+        hs.insert(12);
+        System.out.println(hs);
+
+    }
 }
 
