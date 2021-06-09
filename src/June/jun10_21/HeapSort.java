a666b36dc3252425372df3c5e47a928f7fd57de8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-09 17:25:51 +0530

    add left, right and parent method

--- src/June/jun10_21/HeapSort.java
+++ src/June/jun10_21/HeapSort.java
@@ -17,5 +17,17 @@ public class HeapSort {
         this.currSize = 0;
         this.capacity = capacity;
     }
+
+    public int left(int i) {
+        return (2 * i + 1);
+    }
+
+    public int right(int i) {
+        return (2 * i + 2);
+    }
+
+    public int parent(int i) {
+        return (i - 1) / 2;
+    }
 }
 
