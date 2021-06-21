a31e9133f806b665c36eb4478b8d378096430d27
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-21 10:04:18 +0530

    create DNode.java

---
+++ src/June/jun21_21/two/DNode.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 21-Jun-21
+ *   Time: 10:03 AM
+ *   File: DNode.java
+ */
+
+package June.jun21_21.two;
+
+public class DNode {
+}
+
