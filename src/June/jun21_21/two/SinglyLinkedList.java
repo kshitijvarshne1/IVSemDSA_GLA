e1b1ad5ac2b6e67c01c3e427649cbe2e50b8c8b8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-21 09:20:43 +0530

    create SinglyLinkedList.java

---
+++ src/June/jun21_21/two/SinglyLinkedList.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 21-Jun-21
+ *   Time: 9:20 AM
+ *   File: SInglyLinkedList.java
+ */
+
+package June.jun21_21.two;
+
+public class SinglyLinkedList {
+}
+
