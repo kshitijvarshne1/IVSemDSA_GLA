7f533827129dcdc4cf8a27d9021fe4f6a801cb81
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-21 09:07:52 +0530

    final commmit

--- src/June/jun21_21/one/Main.java
+++ src/June/jun21_21/one/Main.java
@@ -8,5 +8,24 @@
 package June.jun21_21.one;
 
 public class Main {
+    public static void main(String[] args) {
+        SingleDimensionArray a1 = new SingleDimensionArray(5);
+        a1.insert(2, 102);
+        a1.traverse();
+        a1.insert(1, 99);
+        a1.insert(0, 3);
+        a1.insert(4, 43);
+        a1.delete(1);
+        a1.traverse();
+
+        StudentArray s1 = new StudentArray(5);
+        s1.insert(2, new Student(4, "Sam"));
+        s1.traverse();
+        s1.insert(1, new Student(1, "lam"));
+        s1.insert(4, new Student(3, "dude"));
+        s1.deleteStudent(2);
+        s1.traverse();
+
+    }
 }
 
