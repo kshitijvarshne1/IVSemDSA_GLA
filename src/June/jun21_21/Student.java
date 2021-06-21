2da2bd7a031feb97da893e2a2c27d11104c98bc2
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-21 08:55:27 +0530

    add getter , setter and toString method

--- src/June/jun21_21/Student.java
+++ src/June/jun21_21/Student.java
@@ -15,5 +15,30 @@ public class Student {
         this.rollNo = rollNo;
         this.name = name;
     }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
 }
 
