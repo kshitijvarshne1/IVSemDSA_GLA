c62be91be6a1a95022240945656d8b137fde3935
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-21 08:58:59 +0530

    add constructor, insert method

--- src/June/jun21_21/one/StudentArray.java
+++ src/June/jun21_21/one/StudentArray.java
@@ -8,5 +8,20 @@
 package June.jun21_21.one;
 
 public class StudentArray {
+    Student[] arr = null;
+
+    public StudentArray(int size) {
+        arr = new Student[size];
+    }
+
+    public void insert(int index, Student s) {
+        try {
+            if (index < arr.length) {
+                arr[index] = s;
+            }
+        } catch (Exception e) {
+            System.out.println("index out of bound");
+        }
+    }
 }
 
