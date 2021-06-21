401512c47a92a5ed78a4bd90206b748378c117ad
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-21 10:05:15 +0530

    final commit

--- src/June/jun21_21/two/DNode.java
+++ src/June/jun21_21/two/DNode.java
@@ -8,5 +8,38 @@
 package June.jun21_21.two;
 
 public class DNode {
+    private int data;
+    private DNode next;
+    private DNode prev;
+
+    public DNode(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public DNode getNext() {
+        return next;
+    }
+
+    public void setNext(DNode next) {
+        this.next = next;
+    }
+
+    public DNode getPrev() {
+        return prev;
+    }
+
+    public void setPrev(DNode prev) {
+        this.prev = prev;
+    }
 }
 
