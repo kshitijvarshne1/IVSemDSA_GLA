5a8257ef6a2a0e6f25eec31e5ef4e2bf6b9564c1
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-21 10:20:37 +0530

    create DMain.java

---
+++ src/June/jun21_21/two/DMain.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 21-Jun-21
+ *   Time: 10:20 AM
+ *   File: DMain.java
+ */
+
+package June.jun21_21.two;
+
+public class DMain {
+}
+
