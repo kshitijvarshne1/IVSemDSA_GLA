da288ee1cfebc5c1af59fe38fe2a9e6540220e35
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-21 08:54:41 +0530

    add field and constructor

--- src/June/jun21_21/Student.java
+++ src/June/jun21_21/Student.java
@@ -8,5 +8,12 @@
 package June.jun21_21;
 
 public class Student {
+    private int rollNo;
+    private String name;
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
 }
 
