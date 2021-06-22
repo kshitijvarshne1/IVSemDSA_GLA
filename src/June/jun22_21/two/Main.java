0fa77d1e5797cbac58b57828a6a9c4d417324195
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-22 09:27:13 +0530

    final commit

--- src/June/jun22_21/two/Main.java
+++ src/June/jun22_21/two/Main.java
@@ -8,5 +8,14 @@
 package June.jun22_21.two;
 
 public class Main {
+    public static void main(String[] args) {
+        QueueUsingArray qa = new QueueUsingArray(5);
+        qa.enqueue(5);
+        qa.peek();
+        qa.enqueue(33);
+        qa.enqueue(3);
+        qa.dequeue();
+        qa.dequeue();
+    }
 }
 
