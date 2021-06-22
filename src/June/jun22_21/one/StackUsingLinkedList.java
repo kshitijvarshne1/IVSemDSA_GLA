b2aa9d6acea3867809a04ebdbd8504cae6bb0749
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-22 08:50:15 +0530

    final commit

--- src/June/jun22_21/one/StackUsingLinkedList.java
+++ src/June/jun22_21/one/StackUsingLinkedList.java
@@ -8,5 +8,39 @@
 package June.jun22_21.one;
 
 public class StackUsingLinkedList {
+    public Node head;
+
+    public StackUsingLinkedList() {
+        head = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void push(Node newNode) {
+        newNode.setNext(head);
+        head = newNode;
+    }
+
+    public Node peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return head;
+        }
+    }
+
+    public void pop() {
+        if (isEmpty()) {
+            return;
+        } else {
+            head = head.getNext();
+        }
+    }
+
+    public void deleteStack() {
+        head = null;
+    }
 }
 
