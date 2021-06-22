ed7ea71584f21cccd83da79478b6eab719d7f33a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-22 08:30:33 +0530

    implement the push , peek and pop method

--- src/June/jun22_21/one/Main.java
+++ src/June/jun22_21/one/Main.java
@@ -8,5 +8,16 @@
 package June.jun22_21.one;
 
 public class Main {
+    public static void main(String[] args) {
+
+        StackUsingArray sa = new StackUsingArray(5);
+        sa.push(3);
+        sa.push(1);
+        sa.push(6);
+        System.out.println(sa.peek());
+        System.out.println(sa.pop());
+        System.out.println(sa.peek());
+    }
+
 }
 
