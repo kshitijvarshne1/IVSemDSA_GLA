b822f0d19a53103a2f7f843d3caee0987e14c900
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-22 14:42:25 +0530

    final commit

--- src/June/jun22_21/two/Main.java
+++ src/June/jun22_21/two/Main.java
@@ -7,6 +7,8 @@
 
 package June.jun22_21.two;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         QueueUsingArray qa = new QueueUsingArray(5);
@@ -16,14 +18,45 @@ public class Main {
         qa.enqueue(3);
         qa.dequeue();
         qa.dequeue();
-
         QueueUsingLinkedList ql = new QueueUsingLinkedList();
-        ql.enqueue(new Node(new Employee(1, "sam", 1000)));
-        ql.enqueue(new Node(new Employee(2, "john", 2000)));
-        ql.peek();
-        ql.dequeue();
-        ql.peek();
+        Scanner sc = new Scanner(System.in);
+        show();
+        int k = sc.nextInt();
+        while (true) {
+            switch (k) {
+                case 1:
+                    System.out.println("Enter the id");
+                    int id = sc.nextInt();
+                    System.out.println("Enter the name");
+                    String name = sc.next();
+                    System.out.println("Enter the salary:- ");
+                    int salary = sc.nextInt();
+                    ql.enqueue(new Node(new Employee(id, name, salary)));
+                    break;
+                case 2:
+                    ql.dequeue();
+                    break;
+                case 3:
+                    ql.peek();
+                    break;
+                case 4:
+                    System.exit(1);
+                    break;
+                default:
+                    System.out.println("please input 1- 4 ");
+                    break;
+            }
+            show();
+            k = sc.nextInt();
+        }
 
+    }
+
+    public static void show() {
+        System.out.println("1. add a node");
+        System.out.println("2. delete a node");
+        System.out.println("3. show topmost node");
+        System.out.println("4. exit");
     }
 }
 
