9fe0d7ee31b934bc76d484af4f92fc0e3a99b11d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-23 09:11:25 +0530

    add field and constructor

--- src/June/jun23_21/two/Node.java
+++ src/June/jun23_21/two/Node.java
@@ -8,5 +8,14 @@
 package June.jun23_21.two;
 
 public class Node {
+    private int data;
+    private Node left;
+    private Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
 
