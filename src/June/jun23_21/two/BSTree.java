cc34520c31e2cdbd4e4648baa6e9b613dc77c3d9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-23 09:12:24 +0530

    create BSTree.java

---
+++ src/June/jun23_21/two/BSTree.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 23-Jun-21
+ *   Time: 9:12 AM
+ *   File: BSTree.java
+ */
+
+package June.jun23_21.two;
+
+public class BSTree {
+}
+
