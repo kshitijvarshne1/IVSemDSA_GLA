4280b5abb49b0f254befeb650223ae32ac71fa4f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-23 08:34:54 +0530

    add insert method

--- src/June/jun23_21/one/PriorityQueue.java
+++ src/June/jun23_21/one/PriorityQueue.java
@@ -6,13 +6,35 @@
  */
 
 package June.jun23_21.one;
-
+// Two types of pq
+/*
+ * 1. ascending pq
+ * 2. descending pq
+ *
+ * implemented in two ways
+ * 1. simple insertion and special deletion
+ * 2. special insertion and simple deletion
+ * */
 public class PriorityQueue {
     public Node front;
     public Node rear;
 
     public PriorityQueue() {
         this.front = this.rear = null;
+    }
+
+    public void enqueue(Node newNode) {
+        if (rear == null || newNode.getPriority() > front.getPriority()) {
+            newNode.setNext(front);
+            front = newNode;
+        } else {
+            Node tmp = front;
+            while (front.getNext() != null || tmp.getPriority() <= newNode.getPriority()) {
+                tmp = tmp.getNext();
+            }
+            newNode.setNext(tmp.getNext());
+            tmp.setNext(newNode);
+        }
     }
 }
 
