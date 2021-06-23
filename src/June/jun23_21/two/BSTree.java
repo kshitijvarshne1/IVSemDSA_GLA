91225b2926e3787b923e4d303c781d525c443d36
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-23 09:24:44 +0530

    final commit

--- src/June/jun23_21/two/BSTree.java
+++ src/June/jun23_21/two/BSTree.java
@@ -55,6 +55,7 @@ public class BSTree {
 
                 }
             }
+            System.out.println();
         }
     }
 }
--- src/June/jun23_21/two/Main.java
+++ src/June/jun23_21/two/Main.java
@@ -8,5 +8,18 @@
 package June.jun23_21.two;
 
 public class Main {
+    public static void main(String[] args) {
+        BSTree bsTree = new BSTree();
+        bsTree.insert(new Node(100));
+        bsTree.bfs();
+        bsTree.insert(new Node(95));
+        bsTree.bfs();
+        bsTree.insert(new Node(105));
+        bsTree.bfs();
+        bsTree.insert(new Node(85));
+        bsTree.bfs();
+        bsTree.insert(new Node(97));
+        bsTree.bfs();
+    }
 }
 
