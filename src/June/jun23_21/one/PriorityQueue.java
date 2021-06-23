b25df80afd43ec7a90943271e6237d7144189cd8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-23 08:28:21 +0530

    create PriorityQueue.java

---
+++ src/June/jun23_21/one/PriorityQueue.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 23-Jun-21
+ *   Time: 8:28 AM
+ *   File: PriorityQueue.java
+ */
+
+package June.jun23_21.one;
+
+public class PriorityQueue {
+}
+
