4ec4ae416183cf1d1dee84668b7d0e8b562f368d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-23 09:11:52 +0530

    final commit

--- src/June/jun23_21/two/Node.java
+++ src/June/jun23_21/two/Node.java
@@ -17,5 +17,29 @@ public class Node {
         this.left = null;
         this.right = null;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
 }
 
