9e24dd9827385c1ca7ed6077bde7485b88f37f9a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-24 11:49:48 +0530

    final commit

--- src/June/jun03_21/BinarySearch.java
+++ src/June/jun03_21/BinarySearch.java
@@ -8,5 +8,27 @@
 package June.jun03_21;
 
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 6, 8, 42, 52, 100, 311};
+        System.out.println(search(arr, 0, arr.length - 1, 100));
+        System.out.println(search(arr, 0, arr.length - 1, 10));
+    }
+
+    public static boolean search(int[] arr, int l, int r, int ele) {
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == ele) {
+                return true;
+            }
+            if (ele < arr[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+
+        }
+        return false;
+    }
 }
 
