70458e6e843d3548f8fa3fc06375c96301efa850
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-24 13:58:16 +0530

    final commit

--- src/June/jun24_21/DuplicateArray.java
+++ src/June/jun24_21/DuplicateArray.java
@@ -7,9 +7,27 @@
 
 package June.jun24_21;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateArray {
     public static void main(String[] args) {
         int[] arr = {1, 8, 1, 9, 2, 3, 8, 6, 9, 1};
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int i : arr) {
+            if (mp.containsKey(i)) {
+                int count = mp.get(arr[i]);
+                mp.put(arr[i], count + 1);
+            } else {
+                mp.put(i, 1);
+            }
+        }
+        for (var i : mp.entrySet()) {
+            //System.out.println(i.getKey()+" "+i.getValue());
+            if (i.getValue() > 1) {
+                System.out.print(i.getKey() + " ");
+            }
+        }
     }
 }
 
