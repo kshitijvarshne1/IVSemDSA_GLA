741bcd1f3bbdd3d9edd36b9120dfd395fea8dcb9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-25 11:48:47 +0530

    add field and constructor

--- src/June/jun25_21/QueueUsingArray.java
+++ src/June/jun25_21/QueueUsingArray.java
@@ -8,5 +8,18 @@
 package June.jun25_21;
 
 public class QueueUsingArray {
+    int[] arr;
+    int front;
+    int rear;
+
+    public QueueUsingArray(int size) {
+        arr = new int[size];
+        this.front = -1;
+        this.rear = -1;
+    }
+
+    public void pushFront(int element) {
+
+    }
 }
 
