ee4ef911278dcbc2de89c2fe2d4803c77106ee07
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-25 19:14:01 +0530

    final commit

--- src/June/jun25_21/InsertionSort.java
+++ src/June/jun25_21/InsertionSort.java
@@ -7,6 +7,27 @@
 
 package June.jun25_21;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 3, 5, 5, 555};
+        iSort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void iSort(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+    }
+
+
 }
 
