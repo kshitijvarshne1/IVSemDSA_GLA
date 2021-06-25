81692e2f38c0a04a819454cec3518dfca200e385
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-25 19:03:46 +0530

    create InsertionSort.java

---
+++ src/June/jun25_21/InsertionSort.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 25-Jun-21
+ *   Time: 7:03 PM
+ *   File: InsertionSort.java
+ */
+
+package June.jun25_21;
+
+public class InsertionSort {
+}
+
