6291d5533df88c6b0a097f6bc1524d0be3db8ba4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-25 19:02:34 +0530

    final commit

--- src/June/jun25_21/SelectionSort.java
+++ src/June/jun25_21/SelectionSort.java
@@ -7,6 +7,40 @@
 
 package June.jun25_21;
 
+import java.util.Arrays;
+
+/*
+ * break into two half
+ * one is sorted and other is unsorted
+ * Node just find the minimum element and send to the sorted half
+ *
+ * */
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 42, 71, 4, 63, 5};
+        sSort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void sSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                swap(arr, i, min);
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int min) {
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
 }
 
