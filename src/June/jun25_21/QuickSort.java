43c7aefbbf996a628bc69733196a481484bf7b06
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-25 12:42:04 +0530

    final commit

--- src/June/jun25_21/QuickSort.java
+++ src/June/jun25_21/QuickSort.java
@@ -7,10 +7,13 @@
 
 package June.jun25_21;
 
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 99, 4, 2, 77};
         quicksort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void quicksort(int[] arr, int low, int high) {
@@ -24,6 +27,23 @@ public class QuickSort {
 
     private static int partition(int[] arr, int low, int high) {
         int tempPivot = arr[high];
+        // i=> last number which is less than pivot
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < tempPivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
 
